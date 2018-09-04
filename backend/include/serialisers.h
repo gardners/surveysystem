a345bd84995ad8062d86dcc65601106b5e31fc97
Paul Gardner-Stephen <paul@servalproject.org>
2018-09-05 09:28:27 +0930

    add updateanswer command

--- backend/include/serialisers.h
+++ backend/include/serialisers.h
@@ -1,3 +1,7 @@
+#define MISMATCH_IS_NOT_AN_ERROR 0
+#define MISMATCH_IS_AN_ERROR 1
+int compare_questions(struct question *q1, struct question *q2, int mismatchIsError);
+int compare_answers(struct answer *a1, struct answer *a2, int mismatchIsError);
 int serialise_question(struct question *q,char *out,int max_len);
 int serialise_answer(struct answer *a,char *out,int max_len);
 int deserialise_question(char *in,struct question *q);
