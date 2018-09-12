c914c4c5df3e36c252bdc521dd1c2d57a3c58c30
Angus Wallace <angus.wallace@flinders.edu.au>
2018-09-12 13:05:05 +0930

    few extra comments

--- backend/setUpTest.sh
+++ backend/setUpTest.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# set up some testing of the command line interface
+
+# create a symbolic link of the survey question definitions so that we can test them while keeping them outside the public repo
 if [ ! -f ./surveys/foo/current ]; then
 	ln -s /Users/wall0159/code/SleepCompanionREimplementation/engine/SleepCompanion/current ./surveys/foo/
 fi
 
-# set up python
+
+# set up python function by creating symbolic links
 mkdir -p ./python
 if [ ! -f ./python/nextquestion.py ]; then
         ln -s /Users/wall0159/code/SleepCompanionREimplementation/nextquestion.py ./python/nextquestion.py 
         ln -s /Users/wall0159/code/SleepCompanionREimplementation/questionLogic.py ./python/
 fi
 
+# seems to be a bug that the surveycli doesn't actually return a SESSIONID, so get it from the surveys/foo file
 SESSIONID=`ls -1 ./surveys/foo/ | head -n 1`
 #SURVEY_HOME=. ./surveycli newsession foo
 
