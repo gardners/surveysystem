ebd0ff316027a7922932a7e5e44a24ab847cfa45
Paul Gardner-Stephen <paul@servalproject.org>
2018-10-26 15:35:01 +1030

    begin working on kcgi to backend plumbing

--- backend/src/fcgimain.c
+++ backend/src/fcgimain.c
@@ -13,6 +13,43 @@
 #include "survey.h"
 #include "serialisers.h"
 
+enum	page {
+	PAGE_NEWSESSION,
+	PAGE_ADDANSWER,
+	PAGE_UPDATEANWSER,
+	PAGE_NEXTQUESTION,
+	PAGE_DELANSWER,
+	PAGE_DELSESSION,
+	PAGE__MAX
+};
+
+typedef	void (*disp)(struct kreq *);
+
+static void fcgi_newsession(struct kreq *);
+static void fcgi_addanswer(struct kreq *);
+static void fcgi_updateanswer(struct kreq *);
+static void fcgi_nextquestion(struct kreq *);
+static void fcgi_delanswer(struct kreq *);
+static void fcgi_delsession(struct kreq *);
+
+static const disp disps[PAGE__MAX] = {
+  fcgi_newsession,
+  fcgi_addanswer,
+  fcgi_updateanswer,
+  fcgi_nextquestion,
+  fcgi_delanswer,
+  fcgi_delsession
+}
+
+static const char *const pages[PAGE__MAX] = {
+  "newsession",
+  "addanswer",
+  "updateanswer",
+  "nextquestion",
+  "delanswer",
+  "delsession"
+};
+  
 void usage(void)
 {
   fprintf(stderr,
@@ -37,12 +74,17 @@ int main(int argc,char **argv)
     struct kfcgi    *fcgi=NULL;
     enum kcgi_err    er;
     
-    if (KCGI_OK != khttp_fcgi_init(&fcgi, NULL, 0, NULL, 0, 0))
+    if (KCGI_OK != khttp_fcgi_init(&fcgi,
+				   NULL, 0, // Keys for parsing
+				   pages, PAGE__MAX,  // Pages for parsing
+				   PAGE_NEWSESSION))
       {	LOG_ERROR("khttp_fcgi_init() failed.",""); }
 
     if (!fcgi) { LOG_ERROR("fcgi==NULL after call to khttp_fcgi_init()",""); }
-    
+
+    // For each request
     for (;;) {
+      // Parse request
       er = khttp_fcgi_parse(fcgi, &req);
       if (KCGI_EXIT == er) {
 	fprintf(stderr, "khttp_fcgi_parse: terminate\n");
@@ -51,7 +93,8 @@ int main(int argc,char **argv)
 	fprintf(stderr, "khttp_fcgi_parse: error: %d\n", er);
 	break;
       }
-      
+
+      // Emit 200 response
       er = khttp_head(&req, kresps[KRESP_STATUS], 
 		      "%s", khttps[KHTTP_200]);
       if (KCGI_HUP == er) {
@@ -63,7 +106,8 @@ int main(int argc,char **argv)
 	khttp_free(&req);
 	break;
       }
-      
+
+      // Emit mime-type
       er = khttp_head(&req, kresps[KRESP_CONTENT_TYPE], 
 		      "%s", kmimetypes[req.mime]);
       if (KCGI_HUP == er) {
@@ -75,7 +119,8 @@ int main(int argc,char **argv)
 	khttp_free(&req);
 	break;
       }
-      
+
+      // Begin sending body
       er = khttp_body(&req);
       if (KCGI_HUP == er) {
 	fprintf(stderr, "khttp_body: interrupt\n");
@@ -86,7 +131,8 @@ int main(int argc,char **argv)
 	khttp_free(&req);
 	break;
       }
-      
+
+      // Write some stuff in reply
       er = khttp_puts(&req, "Hello, world!\n");
       if (KCGI_HUP == er) {
 	fprintf(stderr, "khttp_puts: interrupt\n");
@@ -97,7 +143,8 @@ int main(int argc,char **argv)
 	khttp_free(&req);
 	break;
       }
-      
+
+      // Close off request
       khttp_free(&req);
     }
 
@@ -111,4 +158,9 @@ int main(int argc,char **argv)
     dump_errors(stderr);
   }
   return retVal;
+}
+
+static void fcgi_newsession(struct kreq *)
+{
+  
 }
