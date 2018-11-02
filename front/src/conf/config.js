c62309446f31c7b8aee7cf2d9ea3719859fba523
Yourgene <vladislav.sokolov@hotmail.fr>
2018-11-02 11:57:21 +1030

    custom properties are managed in a better way

--- front/src/conf/config.js
+++ front/src/conf/config.js
@@ -1,5 +1,25 @@
 export const Configuration = {
-    surveyTheme: 'bootstrap',
+    //back end config
     serverUrl: 'http://localhost',
-    serverPort :'80'
+    serverPort :'80',
+
+    //survey theme
+    surveyTheme: 'bootstrap',
+
+    //custom properties
+    customProperties : [
+        {
+            'basetype' : 'questionbase',
+            'name' : 'id',
+            'type' : 'string'
+        },
+        {
+            'basetype' : 'questionbase',
+            'name' : 'title_text',
+            'type' : 'string'
+        }
+    ]
+
+
+
 };
