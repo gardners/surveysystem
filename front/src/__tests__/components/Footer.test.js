74b4791dee67d91fd87b2fe8c2a52ad9ab72c50d
joerg <sendstuff2bo@gmail.com>
2018-11-15 10:17:45 +1030

    enable test suite, started storage tests

--- front/src/__tests__/components/Footer.test.js
+++ front/src/__tests__/components/Footer.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from "../src/components/Navbar";
+import Footer from '../../components/Footer';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Navbar />, div);
+    ReactDOM.render(<Footer />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
---
+++ front/src/__tests__/storage/LocalStorage.js
@@ -0,0 +1,57 @@
+import Storage from '../../storage/LocalStorage';
+require('jest-localstorage-mock');
+
+const KEY = '__test__';
+
+describe('LocalStorage', () => {
+
+    afterAll(() => {
+        window.localStorage.removeItem(KEY);
+    });
+
+    test('store complex data', () => {
+        const data = [1, 2, 3];
+
+        Storage.set(KEY, data);
+        const res = localStorage.getItem(KEY);
+
+        expect(res).toBe(JSON.stringify(data));
+    });
+
+    test('store stringified JSON', () => {
+        const json = JSON.stringify([1, 2, 3]);
+
+        Storage.set(KEY, json);
+        const res = localStorage.getItem(KEY);
+
+        it('shouldn\'t double encode JSON strings', () => {
+            expect(res).toBe(JSON.stringify(data));
+        })
+
+    });
+
+    test('fetch invalid JSON', () => {
+        const json = '{ invalid }';
+
+        localStorage.setItem(KEY, json);
+        const res = Storage.get(KEY);
+
+        it('should return NULL in case the stored value is invalid json', () => {
+            expect(res).toStrictEqual(null);
+        })
+
+    });
+
+    test('store and fetch undefined data', () => {
+        const data = undefined;
+
+        Storage.set(KEY, data);
+        const res = Storage.get(KEY);
+
+        it('should return NULL in case the stored value is invalid json', () => {
+            expect(res).toStrictEqual(null);
+        })
+
+    });
+
+});
