8ae1b5f038d49da99b103be2ae1d8ebad1a5b5d1
joerg <sendstuff2bo@gmail.com>
2018-11-14 11:08:41 +1030

    disable seviceworker temporarily

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Navbar from "./components/Navbar";
