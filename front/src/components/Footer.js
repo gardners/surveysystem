fadcfdb889e79cf05d401c060796dedc23be52a1
joerg <sendstuff2bo@gmail.com>
2018-11-15 10:31:34 +1030

    fix className prop

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -4,35 +4,35 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <footer class="page-footer font-small special-color-dark pt-4">
+            <footer className="page-footer font-small special-color-dark pt-4">
 
-                <div class="container">
+                <div className="container">
 
 
-                    <ul class="list-unstyled list-inline text-center">
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-fb mx-1">
-                                <i class="fa fa-facebook"> </i>
+                    <ul className="list-unstyled list-inline text-center">
+                        <li className="list-inline-item">
+                            <a className="btn-floating btn-fb mx-1">
+                                <i className="fa fa-facebook"> </i>
                             </a>
                         </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-tw mx-1">
-                                <i class="fa fa-twitter"> </i>
+                        <li className="list-inline-item">
+                            <a className="btn-floating btn-tw mx-1">
+                                <i className="fa fa-twitter"> </i>
                             </a>
                         </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-gplus mx-1">
-                                <i class="fa fa-google-plus"> </i>
+                        <li className="list-inline-item">
+                            <a className="btn-floating btn-gplus mx-1">
+                                <i className="fa fa-google-plus"> </i>
                             </a>
                         </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-li mx-1">
-                                <i class="fa fa-linkedin"> </i>
+                        <li className="list-inline-item">
+                            <a className="btn-floating btn-li mx-1">
+                                <i className="fa fa-linkedin"> </i>
                             </a>
                         </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-dribbble mx-1">
-                                <i class="fa fa-dribbble"> </i>
+                        <li className="list-inline-item">
+                            <a className="btn-floating btn-dribbble mx-1">
+                                <i className="fa fa-dribbble"> </i>
                             </a>
                         </li>
                     </ul>
@@ -42,7 +42,7 @@ class Footer extends Component {
 
 
 
-                <div class="footer-copyright text-center py-3">© 2018 Copyright:
+                <div className="footer-copyright text-center py-3">© 2018 Copyright:
                     <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
                 </div>
 
