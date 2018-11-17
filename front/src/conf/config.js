ab6d5e09d6621d0277036b770e9151cd507aaea5
joerg <sendstuff2bo@gmail.com>
2018-11-17 11:47:17 +1030

    config.env loader

--- front/src/conf/config.js
+++ front/src/conf/config.js
@@ -1,70 +1,8 @@
-export const Configuration = {
-    //back end config
-    serverUrl: 'http://localhost',
-    serverPort :'80',
-    serverBaseUrl : function(){
-        let baseURL =this.serverUrl.concat(':',this.serverPort)
-        return(baseURL)
-    },
+/**
+ * loads configuration according to NODE_ENV
+ * @see https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables
+ * This requires commonjs module loader (no dynamic imports), no worries webpack takes care of that
+ */
 
-    //survey theme
-    surveyTheme: 'bootstrap',
-
-    //custom properties
-    customProperties : [
-        {
-            'basetype' : 'questionbase',
-            'name' : 'id',
-            'type' : 'string',
-            'description' : 'identifies a question between the back and front'
-        },
-        {
-            'basetype' : 'questionbase',
-            'name' : 'title_text',
-            'type' : 'string',
-            'description' : ''
-        }
-    ],
-
-
-    apiCalls : {
-        createNewSession : {
-            method : 'get',
-            path : '/surveyapi/newsession',
-            params :
-                {
-                    surveyid : null
-                }
-        },
-        nextQuestion : {
-            method : 'get',
-            path : '/surveyapi/nextquestion',
-            params :
-                {
-                    sessionid : null
-                }
-        },
-        updateAnswer : {
-            method : 'get',
-            path : '/surveyapi/updateanswer',
-            params :
-                {
-                    sessionid : null,
-                    answer : null
-                }
-        },
-        deleteAnswer : {
-            method : 'get',
-            path : '/surveyapi/delanswer',
-            params :
-                {
-                    sessionid : null,
-                    questionid : null
-                }
-        }
-    }
-
-
-
-
-};
+const config = require(`./config.${process.env.NODE_ENV}.js`);
+module.exports = config;
