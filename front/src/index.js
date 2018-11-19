01fe0ca2414ffe05cb86cfb7cea210348ec30289
joerg <sendstuff2bo@gmail.com>
2018-11-19 13:30:21 +1030

    #25 implement default survey redirect

--- front/src/index.js
+++ front/src/index.js
@@ -1,37 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import registerServiceWorker from './registerServiceWorker';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.css';
+import { Configuration } from './conf/config';
 
+// views
 import Navbar from "./components/Navbar";
 import FlexibleSurvey from "./components/FlexibleSurvey";
 import Page404 from "./components/Page404"
 import Welcome from "./components/Welcome"
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-
+const { surveys, defaultSurveyID } = Configuration;
 
 //rendering the routes
 ReactDOM.render(
     <Navbar />,
     document.getElementById("header"));
 
-
-
 ReactDOM.render(
     <Router>
         <Switch>
-            <Route exact path="/" component={Welcome} />
+
+            <Route exact path="/" render={ props => (surveys.length) ? <Redirect to={ `/survey/${surveys[0]}` } /> : <Welcome /> } />
             <Route path="/survey/:id" component={FlexibleSurvey} />
             <Route path="*" component={Page404} />
+
         </Switch>
     </Router>,
     document.getElementById('app')
 )
-
-
-
-
-//registerServiceWorker();
