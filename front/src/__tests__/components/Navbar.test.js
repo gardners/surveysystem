84ffb6e3c2074634776c716820f85611fb3d0496
joerg <sendstuff2bo@gmail.com>
2018-11-21 15:09:23 +1030

    #20 integrating router in test for navbar component

--- front/src/__tests__/components/Navbar.test.js
+++ front/src/__tests__/components/Navbar.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+
 import Navbar from '../../components/Navbar';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Navbar />, div);
+    ReactDOM.render(
+        <MemoryRouter>
+            <Navbar />
+        </MemoryRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
