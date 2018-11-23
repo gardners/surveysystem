83222c425705b8dadd647ce107f0fc312fbef06e
joerg <sendstuff2bo@gmail.com>
2018-11-23 17:09:07 +1030

    #40 form components (who can be improved)

---
+++ front/src/components/form/TextInput.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TextInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+    }
+
+    handleChange(e) {
+        const { value } = e.target;
+        const { question } = this.props;
+
+        this.setState({
+            value: value
+        });
+        this.props.handleChange(value, question);
+    }
+
+    render() {
+        const { question } = this.props;
+        return (
+            <div className="form-group">
+                <label htmlFor={ question.id }>{ question.title_text }</label>
+                <input
+                    id={ question.id }
+                    name={ question.name }
+                    type="text"
+                    className="form-control"
+                    placeholder={ this.props.placeholder }
+                    onChange={ this.handleChange.bind(this) }
+                />
+            </div>
+        );
+    }
+}
+
+TextInput.defaultProps = {
+    placeholder: null,
+};
+
+TextInput.propTypes = {
+    handleChange: PropTypes.func.isRequired,
+    question: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        title_text: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+    }).isRequired,
+    placeholder: PropTypes.string,
+};
+
+export default TextInput;
