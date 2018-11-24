f6fc07815b81f0c9d71a628b726f3fdaa34b8c2f
joerg <sendstuff2bo@gmail.com>
2018-11-24 17:25:03 +1030

    #40 merge cached state

--- front/src/SurveyManager.js
+++ front/src/SurveyManager.js
@@ -1,3 +1,5 @@
+import Log from './Log';
+
 /**
  * @module SurveyManager Simple state controller for survey progess.
  * There is not much to it as the state is controlled remotely by the backendx
@@ -9,12 +11,16 @@ class SurveyManager {
      * @param {string} surveyID The surveyID returned from backend. Note that ther is no logic attached to the property, it just houses it for convenience
      * @returns {void}
      */
-    constructor(surveyID) {
+    constructor(surveyID, cached = null) {
         this.surveyID = surveyID;
         this.sessionID = null;
         this.step = -1;
         this.finished = false;
         this.questions = []; // QuestionSets
+
+        if (cached) {
+            this.merge(cached);
+        }
     }
 
     /**
@@ -27,13 +33,28 @@ class SurveyManager {
     }
 
     /**
-     * Returns the current question, which is always the last entry in this.questions
-     * @returns {[object]} array of QuestionItems or empty array
+     * Merges a cached session object
+     * @param {object} cached deserialized JSON
+     * @returns {void}
      */
-    current() {
-        const { length } = this.questions;
-        return (length) ? this.questions[length - 1] : [];
+    merge(cached) {
+        // simple surveyID check
+        const surveyID = cached.surveyID || '';
+        if(surveyID !== this.surveyID) {
+            Log.warn(`SessionManger.merge: surveyID supplied values doesn't match ${this.surveyI}`);
+            return;
+        }
+
+        Object.keys(cached).forEach((key) => {
+            const type = typeof(this[key]);
+            if(type === 'undefined' || type === 'function') {
+                return;
+            }
+
+            this[key] = cached[key];
+        });
     }
+
 
     /**
      * Adds a new set of QuestionItems and sets progress flags
@@ -47,6 +68,15 @@ class SurveyManager {
         }
         this.questions.push(questions);
         this.step += 1;
+    }
+
+    /**
+     * Returns the current question, which is always the last entry in this.questions
+     * @returns {[object]} array of QuestionItems or empty array
+     */
+    current() {
+        const { length } = this.questions;
+        return (length) ? this.questions[length - 1] : [];
     }
 
     /**
