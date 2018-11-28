c023c124985cbdda49e1e48680d0bf3a9b9d7068
joerg <sendstuff2bo@gmail.com>
2018-11-28 13:28:34 +1030

    #47 adjustments

--- front/src/components/Surveys.jsx
+++ front/src/components/Surveys.jsx
@@ -13,27 +13,32 @@ class Surveys extends Component {
         const cachedTime = (cache) ? cache.time : '';
 
         return (
-            <div className="container-fluid">
-            <h1>{ surveyProvider }</h1>
-            <p>Survey front page. For tests, try /survey/:id with a valid id to display the survey</p>
-            <h2>Our Surveys</h2>
+            <section>
+                <h1>{ surveyProvider }</h1>
+                <strong>Our Surveys</strong>
 
-            { surveys.map(survey =>
-                <div key={survey} className="panel panel-default">
-                    <div className="panel-body">
-                        <h3>{ survey }</h3>
-                        { (cachedSurveyID === survey) ?
-                            <div>
-                                { (cachedTime) ? <p><small>last access: <i>{ new Date(cachedTime).toLocaleString() }</i>, session ID: { cache.sessionID }</small></p> : 'n/a' }
-                                <Link to={ `/survey/${survey}/${cache.sessionID}` } className="btn btn-primary">Continue</Link>
-                                <Link to={ `/survey/${survey}/new` } onClick={ () => LocalStorage.delete('sessionstate') } className="btn btn-default">Restart</Link>
+                <div className="row">
+
+                    { surveys.map(survey =>
+                        <div className="col col-sm-6">
+                            <div key={survey} className="card">
+                                <div className="card-body">
+                                    <h3 className="card-title">{ survey }</h3>
+                                    { (cachedSurveyID === survey) ?
+                                        <div>
+                                            { (cachedTime) ? <p><small>last access: <i>{ new Date(cachedTime).toLocaleString() }</i>, session ID: { cache.sessionID }</small></p> : 'n/a' }
+                                            <Link to={ `/survey/${survey}/${cache.sessionID}` } className="btn btn-primary">Continue</Link>
+                                            <Link to={ `/survey/${survey}/new` } onClick={ () => LocalStorage.delete('sessionstate') } className="btn btn-default">Restart</Link>
+                                        </div>
+                                        :
+                                    <Link to={ `/survey/${survey}` } className="btn btn-primary">Start</Link> }
+                                </div>
                             </div>
-                            :
-                        <Link to={ `/survey/${survey}` } className="btn btn-primary">Start</Link> }
-                    </div>
+                        </div>
+                    ) }
                 </div>
-            ) }
-            </div>
+
+            </section>
         );
     }
 }
