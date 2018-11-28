fbb98ec9083896e09fd2342d62dcb6e49b5ce1bd
joerg <sendstuff2bo@gmail.com>
2018-11-28 16:33:49 +1030

    #27 amend tests

--- front/src/__tests__/components/HeaderNav.test.js
+++ front/src/__tests__/components/HeaderNav.test.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 
-import Navbar from '../../components/Navbar';
+import HeaderNav from '../../components/HeaderNav';
+
+const location = {
+    pathname: 'test',
+};
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
         <MemoryRouter>
-            <Navbar />
+            <HeaderNav location={ location }/>
         </MemoryRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
