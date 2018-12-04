d60c5a5dcadf6b8cd01c54b9a1e78d7e2547f4bb
joerg <sendstuff2bo@gmail.com>
2018-12-04 18:49:00 +1030

    start timerange

--- front/src/components/form/PeriodRange.jsx
+++ front/src/components/form/PeriodRange.jsx
@@ -1,23 +1,79 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const max = 86400; // 24 hours
+import 'rc-slider/assets/index.css';
+import Tooltip from 'rc-tooltip';
+import Slider from 'rc-slider';
 
-const computeStyles = function() {
+const d2sec = 86400; // 24 hours
+const h2sec = 3600;
 
+const dark = '#212077';
+const light = '#ffc107';
+const gradient = `linear-gradient(to right, ${dark} 15%, ${light} 30%, ${light} 70%, ${dark} 85%)`;
+
+const tableStyle = {
+    display: 'table',
+    width: '100%',
+    padding: '.5em',
+    tableLayout: 'fixed',    /* For cells of equal size */
+    background: gradient,
+};
+
+const cellStyle = function(percent) {
+    const textAlign = ((percent === 50) ? 'center' : (percent < 50) ? 'left' : 'right');
+    return {
+        width: '25%',
+        display: 'table-cell',
+        textAlign,
+        fontSize: '2em',
+    };
+};
+
+const prettyHours = function(sec) {
+    let hours   = Math.floor(sec / 3600);
+    let minutes = Math.floor((sec - (hours * 3600)) / 60);
+    let seconds = sec - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0" + hours;}
+    let t = 'am';
+    if (hours > 12) {
+        hours -= 12;
+        t = 'pm';
+    }
+    if (minutes < 10) {minutes = "0" + minutes;}
+    if (seconds < 10) {seconds = "0" + seconds;}
+    return `${hours}:${minutes} ${t}`;
+};
+
+const Range = Slider.Range;
+
+const Handle = (props) => {
+  const { value, dragging, index, ...restProps } = props;
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={ prettyHours(value) }
+      placement="top"
+      key={ index }
+      trigger={ [] } /* overwrite */
+      visible={ true } /* overwrite */
+      defaultVisible={ true }
+    >
+      <Slider.Handle value={value} {...restProps} />
+    </Tooltip>
+  );
 };
 
 /**
- * range sliders for defining period values (seconds) within 24 hours
+ * range sliders for defining period secondss (seconds) within 24 hours
  */
 class PeriodRange extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: {
-                time_begin: 0,
-                time_end: 0,
-            },
+            time_begin: 0,
+            time_end: 0,
         };
     }
 
@@ -25,11 +81,12 @@ class PeriodRange extends Component {
         const { name, value } = e.target;
         const { question } = this.props;
 
-        const updated = this.state.value;
+        const updated = this.state;
         updated[name] = value;
 
         this.setState({
-            value: updated,
+            seconds: updated,
+            //step: (this.state.step < this.state.vals - 1) ? this.state.vals + 1 : this.state.step,
         });
         // TODO validate here?
         this.props.handleChange(updated, question);
@@ -37,37 +94,31 @@ class PeriodRange extends Component {
 
     render() {
         const { question, timeBeginLabel, timeEndLabel } = this.props;
-        const { time_begin, time_end } = this.state.value;
+        const { time_begin, time_end } = this.state;
 
+        const domain = [0, d2sec];
         return (
             <div className="form-group">
-                <div id={ question.id } className="range">
-                    <label>
-                        <input
-                            type="range"
-                            name="time_begin"
-                            id="time_begin"
-                            min={ 0 }
-                            max={ (time_end)?  time_end : max }
-                            onChange={ this.handleChange.bind(this)}
-                            />
-                        { timeBeginLabel }
-                    </label>
+                <div className={ this.props.className } style={ tableStyle }>
+                    <div style={ cellStyle(0)}><i className="fas fa-moon text-white"></i></div>
+                    <div style={ cellStyle(50) }><i className="fas fa-sun text-white"></i></div>
+                    <div style={ cellStyle(100) }><i className="fas fa-moon text-white"></i></div>
                 </div>
+                <Range
+                    handle= { Handle }
+                    min={ 0 }
+                    max={ d2sec }
+                    defaultValue={ [1000, 3000] }
+                    allowCross={ false }
+                    pushable={ true }
 
-                <div className="range">
-                    <label>
-                        <input
-                            type="range"
-                            name="time_end"
-                            id="time_end"
-                            min={ time_begin }
-                            max={ max }
-                            onChange={ this.handleChange.bind(this)}
-                            />
-                        { timeEndLabel }
-                    </label>
-                </div>
+                    activeDotStyle={ {
+                        backgroundColor: 'blue',
+                    } }
+                    //railStyle = {
+                    //    railStyle
+                    //}
+                />
             </div>
         );
     }
