213eb300a6367ff96b5b588c789a4430b7232f47
joerg <sendstuff2bo@gmail.com>
2018-12-07 14:42:59 +1030

    #38 add number input

---
+++ front/src/components/form/NumberInput.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NumberInput = function(props) {
+    const { question, placeholder, value } = props;
+
+    return (
+        <div className="form-group">
+            <label htmlFor={ question.id }>{ question.title_text }</label>
+            <input
+                id={ question.id }
+                name={ question.name }
+                type="number"
+                className="form-control"
+                placeholder={ placeholder }
+                autoComplete="off"
+                onChange={ (e) => {
+                    const { value } = e.target;
+                    props.handleChange(question, value);
+                } }
+            />
+        </div>
+    );
+};
+
+NumberInput.defaultProps = {
+    placeholder: null,
+};
+
+NumberInput.propTypes = {
+    handleChange: PropTypes.func.isRequired,
+    question: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        title_text: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+    }).isRequired,
+    placeholder: PropTypes.string,
+};
+
+export default NumberInput;
