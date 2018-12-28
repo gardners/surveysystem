34e135067a289ed2a821684822991f957077eb6c
joerg <sendstuff2bo@gmail.com>
2018-12-28 12:38:46 +1030

    #85 add email component

---
+++ front/src/components/form/EmailInput.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const EmailInput = function(props) {
+    const { question, placeholder } = props;
+
+    return (
+        <div className="form-group">
+            <label htmlFor={ question.id }>{ question.title }</label>
+            <input
+                id={ question.id }
+                name={ question.name }
+                type="email"
+                className="form-control"
+                placeholder={ placeholder }
+                autoComplete="off"
+                onChange={ (e) => {
+                    const { value } = e.target;
+                    props.handleChange(question, value);
+                } }
+            />
+        </div>
+    );
+};
+
+EmailInput.defaultProps = {
+    placeholder: null,
+};
+
+EmailInput.propTypes = {
+    handleChange: PropTypes.func.isRequired,
+    question: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        title_text: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+    }).isRequired,
+    placeholder: PropTypes.string,
+};
+
+export default EmailInput;
