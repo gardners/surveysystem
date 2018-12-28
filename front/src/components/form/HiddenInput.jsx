0d102b6505c073c2383be1e72e4a8d0a05baf550
joerg <sendstuff2bo@gmail.com>
2018-12-28 12:07:38 +1030

    #85 add hidden form component

---
+++ front/src/components/form/HiddenInput.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class HiddenInput extends Component {
+
+    // ! immediately invoke answer callback
+    // note: no value and validation! handleChange is invoked immediately with defaultvalue
+    componentDidMount() {
+        const { question,defaultValue } = this.props;
+        this.props.handleChange(question, defaultValue);
+    }
+
+    render() {
+        const { question, defaultValue } = this.props;
+        return (
+            <div className="form-group">
+                <input
+                    id={ question.id }
+                    name={ question.name }
+                    type="hidden"
+                    autoComplete="off"
+                    defaultValue={ defaultValue }
+                />
+            </div>
+        );
+    }
+};
+
+HiddenInput.defaultProps = {
+    defaultValue: null,
+};
+
+HiddenInput.propTypes = {
+    handleChange: PropTypes.func.isRequired,
+    question: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        title_text: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+    }).isRequired,
+
+    // custom
+    defaultValue: PropTypes.string,
+};
+
+export default HiddenInput;
