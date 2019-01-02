9f510299ecc2aba5ee9e7256813d2afeec48783c
joerg <sendstuff2bo@gmail.com>
2019-01-02 12:48:52 +1030

    #91, create dummy validator module

---
+++ front/src/validator/index.js
@@ -0,0 +1,15 @@
+// getAnswer(): eturns Error or serialized value
+// submit:
+// - were all (required) fields filled in? (answers.length === questions.length
+// - does answers collection contain Errors?
+// element
+//  - <element>.checkValidity() : false > Error
+//  - payload serializer: Error
+//  - validator: false > Error
+
+const validateAnswer = function() {
+    // TODO
+    return null;
+};
+
+export { validateAnswer };
