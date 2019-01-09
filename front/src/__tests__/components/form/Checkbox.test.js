f3f28adc7f6d6332b652922d987d60d5374f660f
joerg <sendstuff2bo@gmail.com>
2019-01-09 17:19:38 +1030

    #98 tests for Checkbox component

---
+++ front/src/__tests__/components/form/Checkbox.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
+import Checkbox from '../../../components/form/Checkbox';
+
+let component;
+
+const question = {
+    id: 'q1',
+    name: 'q1',
+    title: 'q1',
+    title_text: 'q1',
+    type: 'CHECKBOX',
+    choices: [ 'uncheckedVal', 'checkedVal' ],
+    unit: '',
+};
+
+beforeAll(() => {
+    component = renderer.create(
+        <Checkbox
+            question={ question }
+            handleChange={ () => {} } />,
+    );
+});
+
+it('renders without crashing', () => {
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+// TODO test  handleChange supplying first choice
