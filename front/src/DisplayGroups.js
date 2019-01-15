1111463cad30f867071f981e83a9cb0dfa39e3ac
joerg <sendstuff2bo@gmail.com>
2019-01-15 17:19:04 +1030

    #102 functional rewrite of DisplayGroups

--- front/src/DisplayGroups.js
+++ front/src/DisplayGroups.js
@@ -1,4 +1,7 @@
-
+/**
+ * Extract the group id from a question id
+ * a group flag inside an id string
+ */
 const getGroupId = function(id) {
     // TODO optimize for performance
      const parts = id.split('__');
@@ -8,40 +11,43 @@ const getGroupId = function(id) {
      return parts.join('__').trim();
 };
 
+/**
+ * Create an array of display groups consisting of questions.
+ * The membership of a question to a group is evaluated by a delimiter pattern within the question id.
+ * @param {object[]} Array of question objects
+ *
+ * @returns {array[]} two dimensional arraus of groups and containing question objects
+ */
 
-class DisplayGroups {
-    constructor(questions) {
-        this.groups = [];
+const makeDisplayGroups = function(questions) {
+    const groups = [];
 
-        let lastid = '';
-        questions.forEach((q) => {
-            const id = getGroupId(q.id);
+    let lastid = '';
+    questions.forEach((q) => {
+        const id = getGroupId(q.id);
 
-            // first
-            if(id && id !== lastid) {
-                this.groups.push([q]);
-                lastid = id;
-                return;
-            }
-
-            //match - middle
-            if(id && id === lastid) {
-                this.groups[this.groups.length - 1].push(q);
-                lastid = id;
-                return;
-            }
-
-            // anything else
-            this.groups.push([q]);
-
-            // cache id
+        // first
+        if(id && id !== lastid) {
+            groups.push([q]);
             lastid = id;
-        });
+            return;
+        }
 
-        //reset cache
-        lastid = '';
-    }
+        //match - middle
+        if(id && id === lastid) {
+            groups[groups.length - 1].push(q);
+            lastid = id;
+            return;
+        }
 
-}
+        // anything else
+        groups.push([q]);
 
-export { DisplayGroups as default, getGroupId };
+        // cache id
+        lastid = id;
+    });
+
+    return groups;
+};
+
+export { makeDisplayGroups, getGroupId };
