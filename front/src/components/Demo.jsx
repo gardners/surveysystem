931352c61e0ef20d0ca016bbb83d331a5ecae23d
joerg <sendstuff2bo@gmail.com>
2019-01-23 16:28:14 +1030

    implement question group

--- front/src/components/Demo.jsx
+++ front/src/components/Demo.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { FormRow, FieldError } from './FormHelpers';
+import { FormRow } from './FormHelpers';
 
 // form elements
 import GeoLocation from './form/GeoLocation';
@@ -23,6 +23,8 @@ import PasswordInput from './form/PasswordInput';
 
 import { serializeQuestionAnswer } from '../serializer';
 
+import Question from './survey/Question';
+
 const Pre = function(props) {
 
     let cls = null;
@@ -42,11 +44,12 @@ Pre.propTypes = {
     data: PropTypes.any
 };
 
-class Question extends Component {
+class Row extends Component {
     constructor(props) {
         super(props);
         this.state = {
             unit: '',
+            appearance: 'default',
             values: {},
         }
     }
@@ -62,12 +65,11 @@ class Question extends Component {
 
     render() {
         const { props } = this;
-        const { values, unit } = this.state;
+        const { values, unit, appearance } = this.state;
 
         const hasAnswers = Object.keys(values).length > 0;
 
         const Component = props.component;
-        const value = values[Component.name] || '';
 
         const question =  {
             id: Component.name,
@@ -84,15 +86,18 @@ class Question extends Component {
         return (
             <div className="card mb-1" >
                 <FormRow className="card-body" legend={ question.name } description={ question.title_text }>
-                    <Component
-                        { ...this.props } question={ question } handleChange={ this.handleChange.bind(this) }
+                    <Question
+                        { ...props }
+                        question={ question }
+                        handleChange={ this.handleChange.bind(this) }
+                        appearance= { appearance }
                     />
-                    <FieldError error={ (value instanceof Error) ? value : null }/>
                 </FormRow>
 
                 <div className="card-footer text-muted">
                     { hasAnswers && <small className="mr-2 float-left text-info" style={ { fontFamily: 'monospace' } }>{ JSON.stringify(Object.values(values)) }</small> }
                     <small className="mr-2 float-left">Type: { this.props.type }</small>
+                    <small className="mr-2 float-right"><input type="checkbox" onChange={(e) => this.setState({ appearance: (e.target.checked) ? 'inline' : 'default' })} /> horizontal display</small>
                     <small className="mr-2 float-right"><input type="checkbox" onChange={(e) => this.setState({ unit: (e.target.checked) ? 'example unit': '' })} /> toggle unit</small>
                 </div>
             </div>
@@ -100,7 +105,7 @@ class Question extends Component {
     }
 };
 
-Question.propTypes = {
+Row.propTypes = {
     component: PropTypes.func.isRequired,
 };
 
@@ -116,59 +121,59 @@ class Demo extends Component {
         return (
             <section>
                 <div>
-                    <Question type={ 'HIDDEN' }       component={ HiddenInput }       unit={ this.state.unit } title_text="text with some <strong>markup</strong> html and an image: <img src='data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'>" defaultVlaue="visited"/>
-                    <Question type={ 'LATLON' }       component={ GeoLocation }       unit={ this.state.unit } withButton={ true } />
-                    <Question type={ 'TIMERANGE' }    component={ PeriodRangeSlider } unit={ this.state.unit }/>
-                    <Question type={ 'FIXEDPOINT' }   component={ DayTimeSlider }     unit={ this.state.unit }/>
-                    <Question type={ 'FIXEDPOINT' }   component={ TimePicker }        unit={ this.state.unit }/>
-                    <Question type={ 'MULTICHOICE' }  component={ CheckboxGroup }     unit={ this.state.unit } choices={ ['This', 'That', 'Another one' ] } defaultValue="Maybe"/>
-                    <Question type={ 'SINGLECHOICE' } component={ RadioGroup }        unit={ this.state.unit } choices={ ['This', 'That', 'Another one' ] } defaultValue="Maybe"/>
-                    <Question type={ 'CHECKBOX' }     component={ Checkbox }          unit={ this.state.unit } choices={ [ 'unchecked value', 'checked value'] }/>
-                    <Question type={ 'TEXT' }         component={ RadioGroup }        unit={ this.state.unit } choices={ ['Yes', 'No', 'Maybe' ] } defaultValue="Maybe"/>
-                    <Question type={ 'SINGLESELECT' } component={ Select }            unit={ this.state.unit } choices={ ['First', 'Second', 'Third' ] } defaultValue="Second"/>
-                    <Question type={ 'MULTISELECT' }  component={ MultiSelect }       unit={ this.state.unit } choices={ ['First', 'Second', 'Third' ] } defaultValue="Second"/>
-                    <Question type={ 'TEXT' }         component={ TextInput }         unit={ this.state.unit }/>
-                    <Question type={ 'INT' }          component={ NumberInput }       unit={ this.state.unit }/>
-                    <Question type={ 'TEXT' }         component={ Textarea }          unit={ this.state.unit }/>
-                    <Question type={ 'FIXEDPOINT' }   component={ RadioMatrix }       unit={ this.state.unit }
+                    <Row type={ 'HIDDEN' }       component={ HiddenInput }       unit={ this.state.unit } title_text="text with some <strong>markup</strong> html and an image: <img src='data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'>" defaultVlaue="visited"/>
+                    <Row type={ 'LATLON' }       component={ GeoLocation }       unit={ this.state.unit } withButton={ true } />
+                    <Row type={ 'TIMERANGE' }    component={ PeriodRangeSlider } unit={ this.state.unit }/>
+                    <Row type={ 'FIXEDPOINT' }   component={ DayTimeSlider }     unit={ this.state.unit }/>
+                    <Row type={ 'FIXEDPOINT' }   component={ TimePicker }        unit={ this.state.unit }/>
+                    <Row type={ 'MULTICHOICE' }  component={ CheckboxGroup }     unit={ this.state.unit } choices={ ['This', 'That', 'Another one' ] } defaultValue="Maybe"/>
+                    <Row type={ 'SINGLECHOICE' } component={ RadioGroup }        unit={ this.state.unit } choices={ ['This', 'That', 'Another one' ] } defaultValue="Maybe"/>
+                    <Row type={ 'CHECKBOX' }     component={ Checkbox }          unit={ this.state.unit } choices={ [ 'Unchecked!', 'Checked!'] }/>
+                    <Row type={ 'TEXT' }         component={ RadioGroup }        unit={ this.state.unit } choices={ ['Yes', 'No', 'Maybe' ] } defaultValue="Maybe"/>
+                    <Row type={ 'SINGLESELECT' } component={ Select }            unit={ this.state.unit } choices={ ['First', 'Second', 'Third' ] } defaultValue="Second"/>
+                    <Row type={ 'MULTISELECT' }  component={ MultiSelect }       unit={ this.state.unit } choices={ ['First', 'Second', 'Third' ] } defaultValue="Second"/>
+                    <Row type={ 'TEXT' }         component={ TextInput }         unit={ this.state.unit }/>
+                    <Row type={ 'INT' }          component={ NumberInput }       unit={ this.state.unit }/>
+                    <Row type={ 'TEXT' }         component={ Textarea }          unit={ this.state.unit }/>
+                    <Row type={ 'FIXEDPOINT' }   component={ RadioMatrix }       unit={ this.state.unit }
                         questions={ [
                             {
                                 id: 'question1',
                                 name: 'question1',
                                 type: 'FIXEDPOINT',
                                 title: '1',
-                                title_text: 'Question 1 text',
+                                title_text: 'Row 1 text',
                                 choices: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
                                 unit: this.state.unit,
                             }, {
                                 id: 'question2',
                                 name: 'question2',
                                 type: 'FIXEDPOINT',
-                                title: 'Questionfghhd 2',
-                                title_text: 'Question 2 text',
+                                title: 'Rowfghhd 2',
+                                title_text: 'Row 2 text',
                                 choices: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
                                 unit: this.state.unit,
                             },{
                                 id: 'question3',
                                 name: 'question3',
                                 type: 'FIXEDPOINT',
-                                title: 'Question 3',
-                                title_text: 'Question 3 text',
+                                title: 'Row 3',
+                                title_text: 'Row 3 text',
                                 choices: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
                                 unit: this.state.unit,
                             },{
                                 id: 'question4',
                                 name: 'question4',
                                 type: 'FIXEDPOINT',
-                                title: 'Question 4',
-                                title_text: 'Question 4 text',
+                                title: 'Row 4',
+                                title_text: 'Row 4 text',
                                 choices: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
                                 unit: this.state.unit,
                             }
                         ] }
                     />
-                    <Question type={ 'EMAIL' }        component={ EmailInput }           unit={ this.state.unit }/>
-                    <Question type={ 'PASSWORD' }     component={ PasswordInput }        unit={ this.state.unit }/>
+                    <Row type={ 'EMAIL' }        component={ EmailInput }           unit={ this.state.unit }/>
+                    <Row type={ 'PASSWORD' }     component={ PasswordInput }        unit={ this.state.unit }/>
                 </div>
             </section>
         );
