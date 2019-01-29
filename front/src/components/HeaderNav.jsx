d53096554f05e651b6fb22b433f3fe96761b260d
joerg <sendstuff2bo@gmail.com>
2019-01-29 16:52:42 +1030

    #177 receive configuration via props, convert to functional component

--- front/src/components/HeaderNav.jsx
+++ front/src/components/HeaderNav.jsx
@@ -1,5 +1,7 @@
-//Basic Navbar made with bootstrap
-import React, { Component } from 'react';
+//Basic HeaderNav made with bootstrap
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Link } from 'react-router-dom';
 
 import ThemePicker from './ThemePicker';
@@ -17,38 +19,46 @@ const toggle = function(el){
     el.className += ' show';
 };
 
-class Navbar extends Component {
-    render() {
-        return (
-            <header>
-                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark shadow-sm">
-                    <span className="navbar-brand align-items-center">Survey Project</span>
+const HeaderNav = function({ location, surveys, surveyProvider, siteName}) {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark shadow-sm">
+                <span className="navbar-brand align-items-center">{ siteName }</span>
 
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"
-                    onClick={ () => toggle(document.getElementById('header-nav--collapse')) }>
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                <button
+                    className="navbar-toggler"
+                    type="button" data-toggle="collapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"
+                    onClick={ () => toggle(document.getElementById('header-nav--collapse')) }
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                    <div className="collapse navbar-collapse" id="header-nav--collapse">
-                        <ul className="navbar-nav mr-auto">
-                            <li className={ (/^\/surveys/.test(this.props.location.pathname)) ? 'nav-item active' : 'nav-item' }>
+                <div className="collapse navbar-collapse" id="header-nav--collapse">
+                    <ul className="navbar-nav mr-auto">
+                    {
+                        surveys.length > 1 &&
+                            <li className={ (/^\/surveys/.test(location.pathname)) ? 'nav-item active' : 'nav-item' }>
                                 <Link className="nav-link" to="/surveys">Survey List</Link>
                             </li>
-                        </ul>
+                    }
+                    </ul>
 
-                        <ul className="navbar-nav mt-2 mt-md-0">
-                            <li className={ (/^\/demo/.test(this.props.location.pathname)) ? 'nav-item active my-2 my-sm-0' : 'nav-item my-2 my-sm-0' }>
-                                <Link className="nav-link" to="/demo">Demo form</Link>
-                            </li>
-                            <ThemePicker className="nav-item active my-2 my-sm-0" />
-                        </ul>
-                    </div>
-                </nav>
-            </header>
-        );
-    }
-}
+                    <ul className="navbar-nav mt-2 mt-md-0">
+                        <li className={ (/^\/demo/.test(location.pathname)) ? 'nav-item active my-2 my-sm-0' : 'nav-item my-2 my-sm-0' }>
+                            <Link className="nav-link" to="/demo">Demo form</Link>
+                        </li>
+                        <ThemePicker className="nav-item active my-2 my-sm-0" />
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
 
-Navbar.propTypes = {};
+HeaderNav.propTypes = {
+    surveys: PropTypes.arrayOf(PropTypes.string).isRequired,
+    surveyProvider: PropTypes.string.isRequired,
+    siteName: PropTypes.string.isRequired,
+};
 
-export default Navbar;
+export default HeaderNav;
