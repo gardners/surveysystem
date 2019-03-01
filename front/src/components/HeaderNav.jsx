aded15619b00ca3c681f2f8b9ffb5b31e99537a8
joerg <sendstuff2bo@gmail.com>
2019-03-01 15:20:27 +1030

    header nav

--- front/src/components/HeaderNav.jsx
+++ front/src/components/HeaderNav.jsx
@@ -23,7 +23,7 @@ const HeaderNav = function({ location, surveys, surveyProvider, siteName}) {
     return (
         <header>
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark shadow-sm">
-                <span className="navbar-brand align-items-center">{ siteName }</span>
+                <Link to="/" className="navbar-brand align-items-center">{ siteName }</Link>
 
                 <button
                     className="navbar-toggler"
@@ -41,12 +41,12 @@ const HeaderNav = function({ location, surveys, surveyProvider, siteName}) {
                                 <Link className="nav-link" to="/surveys">Survey List</Link>
                             </li>
                     }
-                    </ul>
-
-                    <ul className="navbar-nav mt-2 mt-md-0">
                         <li className={ (/^\/demo/.test(location.pathname)) ? 'nav-item active my-2 my-sm-0' : 'nav-item my-2 my-sm-0' }>
                             <Link className="nav-link" to="/demo">Demo form</Link>
                         </li>
+                    </ul>
+
+                    <ul className="navbar-nav mt-2 mt-md-0">
                         <ThemePicker className="nav-item active my-2 my-sm-0" />
                     </ul>
                 </div>
