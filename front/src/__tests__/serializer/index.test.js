384ebdd8cb3ca9093cc905e40146adbbbfdb1bca
joerg <sendstuff2bo@gmail.com>
2019-06-04 10:47:46 +0930

    #186, update frontend tests

--- front/src/__tests__/serializer/index.test.js
+++ front/src/__tests__/serializer/index.test.js
@@ -20,14 +20,14 @@ describe('serializeAnswer: to CSV row', () => {
 
     test('type: text', () => {
         //positive
-        expect(serializeAnswer('textid', { text: 'T' })).toBe('textid:T:0:0:0:0:0:0:0:');
-        expect(serializeAnswer('textid', { text: 1.24 })).toBe('textid:1.24:0:0:0:0:0:0:0:');
+        expect(serializeAnswer('textid', { text: 'T' })).toBe('textid:T:0:0:0:0:0:0:0::0');
+        expect(serializeAnswer('textid', { text: 1.24 })).toBe('textid:1.24:0:0:0:0:0:0:0::0');
 
-        expect(serializeAnswer('textid', { text: new String('T') })).toBe('textid:\"T\":0:0:0:0:0:0:0:');
+        expect(serializeAnswer('textid', { text: new String('T') })).toBe('textid:\"T\":0:0:0:0:0:0:0::0');
         // tests json and escaping quotes and colons
-        expect(serializeAnswer('textid', { text: '{ "isJSON": true }' })).toBe('textid:{ \"isJSON\"\: true }:0:0:0:0:0:0:0:');
-        expect(serializeAnswer('textid', { text: "Tom's" })).toBe("textid:Tom\'s:0:0:0:0:0:0:0:");
-        expect(serializeAnswer('textid', { text: '  trim text  ' })).toBe('textid:trim text:0:0:0:0:0:0:0:');
+        expect(serializeAnswer('textid', { text: '{ "isJSON": true }' })).toBe('textid:{ \"isJSON\"\: true }:0:0:0:0:0:0:0::0');
+        expect(serializeAnswer('textid', { text: "Tom's" })).toBe("textid:Tom\'s:0:0:0:0:0:0:0::0");
+        expect(serializeAnswer('textid', { text: '  trim text  ' })).toBe('textid:trim text:0:0:0:0:0:0:0::0');
         // negative
         expect(serializeAnswer('textid')).toBeInstanceOf(Error);
         expect(serializeAnswer('textid', null)).toBeInstanceOf(Error);
@@ -41,11 +41,11 @@ describe('serializeAnswer: to CSV row', () => {
 
     test('type: value', () => {
         //positive
-        expect(serializeAnswer('numberid', { value: 0 })).toBe('numberid::0:0:0:0:0:0:0:');
-        expect(serializeAnswer('numberid', { value: 1 })).toBe('numberid::1:0:0:0:0:0:0:');
-        expect(serializeAnswer('numberid', { value: 0.1 })).toBe('numberid::0.1:0:0:0:0:0:0:');
-        expect(serializeAnswer('numberid', { value: '123' })).toBe('numberid::123:0:0:0:0:0:0:');
-        expect(serializeAnswer('numberid', { value: new Number(123) })).toBe('numberid::123:0:0:0:0:0:0:');
+        expect(serializeAnswer('numberid', { value: 0 })).toBe('numberid::0:0:0:0:0:0:0::0');
+        expect(serializeAnswer('numberid', { value: 1 })).toBe('numberid::1:0:0:0:0:0:0::0');
+        expect(serializeAnswer('numberid', { value: 0.1 })).toBe('numberid::0.1:0:0:0:0:0:0::0');
+        expect(serializeAnswer('numberid', { value: '123' })).toBe('numberid::123:0:0:0:0:0:0::0');
+        expect(serializeAnswer('numberid', { value: new Number(123) })).toBe('numberid::123:0:0:0:0:0:0::0');
         // negative
         expect(serializeAnswer('numberid')).toBeInstanceOf(Error);
         expect(serializeAnswer('numberid', null)).toBeInstanceOf(Error);
@@ -60,9 +60,9 @@ describe('serializeAnswer: to CSV row', () => {
 
     test('type: lat', () => {
         // positive
-        expect(serializeAnswer('latid', { lat: '0' })).toBe('latid::0:0:0:0:0:0:0:');
-        expect(serializeAnswer('latid', { lat: '  1.0   ' })).toBe('latid::0:1:0:0:0:0:0:');
-        expect(serializeAnswer('latid', { lat: 3.14 })).toBe('latid::0:3.14:0:0:0:0:0:');
+        expect(serializeAnswer('latid', { lat: '0' })).toBe('latid::0:0:0:0:0:0:0::0');
+        expect(serializeAnswer('latid', { lat: '  1.0   ' })).toBe('latid::0:1:0:0:0:0:0::0');
+        expect(serializeAnswer('latid', { lat: 3.14 })).toBe('latid::0:3.14:0:0:0:0:0::0');
         // negative
         expect(serializeAnswer('latid')).toBeInstanceOf(Error);
         expect(serializeAnswer('latid', undefined)).toBeInstanceOf(Error);
@@ -77,9 +77,9 @@ describe('serializeAnswer: to CSV row', () => {
 
     test('type: lon', () => {
         // positive
-        expect(serializeAnswer('lonid', { lon: '0' })).toBe('lonid::0:0:0:0:0:0:0:');
-        expect(serializeAnswer('lonid', { lon: '  1.0   ' })).toBe('lonid::0:0:1:0:0:0:0:');
-        expect(serializeAnswer('lonid', { lon: 3.14 })).toBe('lonid::0:0:3.14:0:0:0:0:');
+        expect(serializeAnswer('lonid', { lon: '0' })).toBe('lonid::0:0:0:0:0:0:0::0');
+        expect(serializeAnswer('lonid', { lon: '  1.0   ' })).toBe('lonid::0:0:1:0:0:0:0::0');
+        expect(serializeAnswer('lonid', { lon: 3.14 })).toBe('lonid::0:0:3.14:0:0:0:0::0');
         // negative
         expect(serializeAnswer('lonid')).toBeInstanceOf(Error);
         expect(serializeAnswer('lonid', null)).toBeInstanceOf(Error);
@@ -101,13 +101,13 @@ describe('serializeAnswerValue: to CSV row', () => {
 
     test('type: text', () => {
         //positive
-        expect(serializeAnswerValue('textid', 'T', 'text')).toBe('textid:T:0:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('textid', 1.24, 'text')).toBe('textid:1.24:0:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('textid', new String('T'), 'text')).toBe('textid:\"T\":0:0:0:0:0:0:0:');
+        expect(serializeAnswerValue('textid', 'T', 'text')).toBe('textid:T:0:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('textid', 1.24, 'text')).toBe('textid:1.24:0:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('textid', new String('T'), 'text')).toBe('textid:\"T\":0:0:0:0:0:0:0::0');
         // tests json and escaping quotes and colons
-        expect(serializeAnswerValue('textid', '{ "isJSON": true }', 'text')).toBe('textid:{ \"isJSON\"\: true }:0:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('textid', "Tom's", 'text')).toBe('textid:Tom\'s:0:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('textid', '  trim text  ', 'text')).toBe('textid:trim text:0:0:0:0:0:0:0:');
+        expect(serializeAnswerValue('textid', '{ "isJSON": true }', 'text')).toBe('textid:{ \"isJSON\"\: true }:0:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('textid', "Tom's", 'text')).toBe('textid:Tom\'s:0:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('textid', '  trim text  ', 'text')).toBe('textid:trim text:0:0:0:0:0:0:0::0');
         // negative
         expect(serializeAnswerValue('textid', '', 'text')).toBeInstanceOf(Error);
         expect(serializeAnswerValue('textid', null, 'text')).toBeInstanceOf(Error);
@@ -117,11 +117,11 @@ describe('serializeAnswerValue: to CSV row', () => {
 
     test('type: value', () => {
         //positive
-        expect(serializeAnswerValue('numberid', 0, 'value')).toBe('numberid::0:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('numberid', 1, 'value')).toBe('numberid::1:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('numberid', 0.1, 'value')).toBe('numberid::0.1:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('numberid', '123', 'value')).toBe('numberid::123:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('numberid', new Number(123), 'value')).toBe('numberid::123:0:0:0:0:0:0:');
+        expect(serializeAnswerValue('numberid', 0, 'value')).toBe('numberid::0:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('numberid', 1, 'value')).toBe('numberid::1:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('numberid', 0.1, 'value')).toBe('numberid::0.1:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('numberid', '123', 'value')).toBe('numberid::123:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('numberid', new Number(123), 'value')).toBe('numberid::123:0:0:0:0:0:0::0');
         // negative
         expect(serializeAnswerValue('numberid', NaN, 'value')).toBeInstanceOf(Error);
         expect(serializeAnswerValue('numberid', Infinity, 'value')).toBeInstanceOf(Error);
@@ -132,10 +132,10 @@ describe('serializeAnswerValue: to CSV row', () => {
 
     test('type: lat', () => {
         // positive
-        expect(serializeAnswerValue('latid', '0', 'lat')).toBe('latid::0:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('latid', '1.0,', 'lat')).toBe('latid::0:1:0:0:0:0:0:');
-        expect(serializeAnswerValue('latid', '  1.0  ', 'lat')).toBe('latid::0:1:0:0:0:0:0:');
-        expect(serializeAnswerValue('latid', 3.14, 'lat')).toBe('latid::0:3.14:0:0:0:0:0:');
+        expect(serializeAnswerValue('latid', '0', 'lat')).toBe('latid::0:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('latid', '1.0,', 'lat')).toBe('latid::0:1:0:0:0:0:0::0');
+        expect(serializeAnswerValue('latid', '  1.0  ', 'lat')).toBe('latid::0:1:0:0:0:0:0::0');
+        expect(serializeAnswerValue('latid', 3.14, 'lat')).toBe('latid::0:3.14:0:0:0:0:0::0');
         // negative
         expect(serializeAnswerValue('latid', 'string', 'lat')).toBeInstanceOf(Error);
         expect(serializeAnswerValue('latid', undefined, 'lat')).toBeInstanceOf(Error);
@@ -145,10 +145,10 @@ describe('serializeAnswerValue: to CSV row', () => {
 
     test('type: lon', () => {
         // positive
-        expect(serializeAnswerValue('lonid', '0', 'lon')).toBe('lonid::0:0:0:0:0:0:0:');
-        expect(serializeAnswerValue('lonid', '1.0,', 'lon')).toBe('lonid::0:0:1:0:0:0:0:');
-        expect(serializeAnswerValue('lonid', '  1.0  ', 'lon')).toBe('lonid::0:0:1:0:0:0:0:');
-        expect(serializeAnswerValue('lonid', 3.14, 'lon')).toBe('lonid::0:0:3.14:0:0:0:0:');
+        expect(serializeAnswerValue('lonid', '0', 'lon')).toBe('lonid::0:0:0:0:0:0:0::0');
+        expect(serializeAnswerValue('lonid', '1.0,', 'lon')).toBe('lonid::0:0:1:0:0:0:0::0');
+        expect(serializeAnswerValue('lonid', '  1.0  ', 'lon')).toBe('lonid::0:0:1:0:0:0:0::0');
+        expect(serializeAnswerValue('lonid', 3.14, 'lon')).toBe('lonid::0:0:3.14:0:0:0:0::0');
         // negative
         expect(serializeAnswerValue('lonid', 'string', 'lon')).toBeInstanceOf(Error);
         expect(serializeAnswerValue('lonid', undefined, 'lon')).toBeInstanceOf(Error);
