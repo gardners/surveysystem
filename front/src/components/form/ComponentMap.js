ac62c03dcfd4113b1306d00a12532dc07194d8ca
joerg <sendstuff2bo@gmail.com>
2019-06-27 15:21:58 +0930

    add missing component map (demo)

---
+++ front/src/components/form/ComponentMap.js
@@ -0,0 +1,44 @@
+/**
+ * A complete map of form element components
+ * (note that component names are being minified during build)
+ */
+
+import DeviceLocation from '../form/DeviceLocation';
+import PeriodRangeSlider from '../form/PeriodRangeSlider';
+import DayTimeSlider from '../form/DayTimeSlider';
+import CheckboxGroup from '../form/CheckboxGroup';
+import Checkbox from '../form/Checkbox';
+import RadioGroup from '../form/RadioGroup';
+import TextInput from '../form/TextInput';
+import NumberInput from '../form/NumberInput';
+import Textarea from '../form/Textarea';
+import Select from '../form/Select';
+import MultiSelect from '../form/MultiSelect';
+import TimePicker from '../form/TimePicker';
+import RadioMatrix from '../form/RadioMatrix';
+import HiddenInput from '../form/HiddenInput';
+import EmailInput from '../form/EmailInput';
+import PasswordInput from '../form/PasswordInput';
+import DaytimeSequence from '../form/DaytimeSequence';
+import DaytimeQuestionGroup from '../form/DaytimeQuestionGroup';
+
+export default {
+    'DeviceLocation': DeviceLocation,
+    'PeriodRangeSlider': PeriodRangeSlider,
+    'DayTimeSlider': DayTimeSlider,
+    'CheckboxGroup': CheckboxGroup,
+    'Checkbox': Checkbox,
+    'RadioGroup': RadioGroup,
+    'TextInput': TextInput,
+    'NumberInput': NumberInput,
+    'Textarea': Textarea,
+    'Select': Select,
+    'MultiSelect': MultiSelect,
+    'TimePicker': TimePicker,
+    'RadioMatrix': RadioMatrix,
+    'HiddenInput': HiddenInput,
+    'EmailInput': EmailInput,
+    'PasswordInput': PasswordInput,
+    'DaytimeSequence': DaytimeSequence,
+    'DaytimeQuestionGroup': DaytimeQuestionGroup,
+};
