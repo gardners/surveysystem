c2d7bf2ff4776f73711e434cc1f57a86d4aec853
joerg <sendstuff2bo@gmail.com>
2019-06-27 15:18:17 +0930

    refactor media query

--- front/src/Media.js
+++ front/src/Media.js
@@ -1,70 +1,85 @@
 // Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
- /**
-  * @var Breakpoints A map of bootstrap 4 breakpoints { flag: pixels }
-  * @see bootstrapp/scss/mixins/_breakpoints.scss
-  * (xs: 0, sm: 576px, md: 768px, lg: 992px, xl: 1200px)
-  * @see bootstrap.css :root
-  *     --breakpoint-xs: 0;
-  *     --breakpoint-sm: 576px;
-  *     --breakpoint-md: 768px;
-  *     --breakpoint-lg: 992px;
-  *     --breakpoint-xl: 1200px;
-  */
-
-const BreakPoints = [
-    ['xs', 0],
-    ['sm', 576],
-    ['md', 768],
-    ['lg', 992],
-    ['xl', 1200],
-];
-
 /**
- * Checks if a given bootstrap media-query breakpoint applies and returns it.
- *
- * @returns {bool}
+ * @var Breakpoints A map of bootstrap 4 breakpoints { flag: pixels }
+ * @see bootstrapp/scss/mixins/_breakpoints.scss
+ * (xs: 0, sm: 576px, md: 768px, lg: 992px, xl: 1200px)
+ * @see bootstrap.css :root
+ *     --breakpoint-xs: 0;
+ *     --breakpoint-sm: 576px;
+ *     --breakpoint-md: 768px;
+ *     --breakpoint-lg: 992px;
+ *     --breakpoint-xl: 1200px;
  */
 
-const getMediaBreakpoint = function() {
-    const { length } = BreakPoints;
-    let last = 'xs';
-    let point;
+const DEFAULT_BREAKPOINT = 'md';
 
-    for (let i = 0; i < length; i += 1) {
-        point = BreakPoints[i][0];
-        if (!matchesBreakpoint(point) ) {
-            return last;
-        }
-        last = point;
+const xs = 0;
+const sm = 576;
+const md = 768;
+const lg = 992;
+const xl = 1200;
+
+const order = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+let breakpoint = DEFAULT_BREAKPOINT;
+
+const register = function (bp, callback) {
+    breakpoint = bp;
+    if(typeof callback === 'function') {
+        callback(bp);
+    }
+}
+
+const testMediaBreakpoint = function(callback) {
+    // IE < 9, react test renderer
+    if (typeof window.matchMedia !== 'function') {
+        return 'NONE';
     }
 
-    return last;
+    // test from high to low and register;
+    let m;
+
+    m = window.matchMedia(`(min-width: ${xl}px`);
+    if (m && m.matches) {
+        register('xl',callback);
+        return;
+    }
+
+    m = window.matchMedia(`(min-width: ${lg}px`);
+    if (m && m.matches) {
+        register('lg',callback);
+        return;
+    }
+
+    m = window.matchMedia(`(min-width: ${md}px`);
+    if (m && m.matches) {
+        register('md',callback);
+        return;
+    }
+
+    m = window.matchMedia(`(min-width: ${sm}px`);
+    if (m && m.matches) {
+        register('sm',callback);
+        return;
+    }
+
+    m = window.matchMedia(`(min-width: ${xs}px`);
+    if (m && m.matches) {
+        register('xs',callback);
+        return;
+    }
+
+    console.warn('testMediaBreakpoint: No media query breakpoint found');
 };
 
-/**
- * Checks if a given bootstrap media-query breakpoint applies and returns it.
- *
- * @returns {bool}
- */
-
-const matchesBreakpoint = function(bp) {
-    const hits = BreakPoints.filter(point => point[0] === bp);
-
-    if (!hits.length) {
-        return false;
-    }
-
-    // IE < 9, react test renderer
-    if (typeof matchMedia !== 'function') {
-        return false;
-    }
-
-    return window.matchMedia(`(min-width: ${hits[0][1]}px`).matches; // leave leading bracket!
+const isBreakpointAbove = function(bp) {
+    return order.indexOf(breakpoint) >= order.indexOf(bp);
 };
 
 
 export {
-    getMediaBreakpoint,
-    matchesBreakpoint,
+    DEFAULT_BREAKPOINT,
+    testMediaBreakpoint,
+    isBreakpointAbove,
 };
