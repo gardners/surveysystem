13248fcf7a8e4121fc669e270ed530cc4de99e89
joerg <sendstuff2bo@gmail.com>
2019-06-27 15:21:01 +0930

    introduce AppContext with media breakpoints

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect, withRouter } from 'react-router-dom';
+
+import { DEFAULT_BREAKPOINT, testMediaBreakpoint, isBreakpointAbove } from '../Media';
+import { AppContext } from '../AppContext';
 
 // scaffolding
 import HeaderNav from './HeaderNav';
@@ -23,30 +26,56 @@ const surveys = process.env.REACT_APP_SURVEY_LIST.split(',').map(name => name.tr
 const surveyProvider = process.env.REACT_APP_SURVEY_PROVIDER.trim();
 const siteName = process.env.REACT_APP_SITE_NAME.trim();
 
-const App = function(props) {
-    return (
-        <Router>
-            <React.Fragment>
-                <Navigation surveys={ surveys } siteName={ siteName } surveyProvider={ surveyProvider }/>
-                <main className="container" style={{ marginTop: '60px' /*fixed header*/ }}>
-                    <Switch>
-                        <Route exact path="/" render={ props => (surveys.length) ? <Redirect to={ `/survey/${surveys[0]}` } /> : <Surveys /> } />
-                        {
-                            surveys.length > 1 &&
-                                <Route path="/surveys" render={ () => <Surveys surveys={ surveys } surveyProvider={ surveyProvider } /> } />
-                        }
-                        <Route path="/demo/form/:component?" component={ Demo } />
-                        <Route path="/demo/analyse" component={ DemoAnalysis } />
-                        <Route path="/demo/manifest" component={ DemoManifest } />
-                        <Route path="/analyse/:id/:sessionID" component={ Analysis } />
-                        <Route path="/survey/:id/:sessionID?" component={ Survey } />
-                        <Route path="*" component={ Page404 } />
-                    </Switch>
-                </main>
-                <Footer surveyProvider={ surveyProvider } />
-            </React.Fragment>
-        </Router>
-    );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: {
+                breakpoint: DEFAULT_BREAKPOINT,
+                isBreakpointAbove,
+            }
+        }
+        window.addEventListener('resize', this.onWindowResize.bind(this));
+    }
+
+    onWindowResize() {
+        testMediaBreakpoint(bp => {
+            if(bp !== this.state.breakpoint) {
+                this.setState({
+                    breakpoint: bp,
+                });
+            }
+        });
+    }
+
+    render() {
+        const { value } = this.state;
+
+        return (
+            <Router>
+                <AppContext.Provider value={ value }>
+                    <Navigation surveys={ surveys } siteName={ siteName } surveyProvider={ surveyProvider }/>
+                    <main className="container" style={{ marginTop: '60px' /*fixed header*/ }}>
+                        <Switch>
+                            <Route exact path="/" render={ props => (surveys.length) ? <Redirect to={ `/survey/${surveys[0]}` } /> : <Surveys /> } />
+                            {
+                                surveys.length > 1 &&
+                                    <Route path="/surveys" render={ () => <Surveys surveys={ surveys } surveyProvider={ surveyProvider } /> } />
+                            }
+                            <Route path="/demo/form/:component?" component={ Demo } />
+                            <Route path="/demo/analyse" component={ DemoAnalysis } />
+                            <Route path="/demo/manifest" component={ DemoManifest } />
+                            <Route path="/analyse/:id/:sessionID" component={ Analysis } />
+                            <Route path="/survey/:id/:sessionID?" component={ Survey } />
+                            <Route path="*" component={ Page404 } />
+                        </Switch>
+                    </main>
+                    <Footer surveyProvider={ surveyProvider } />
+                </AppContext.Provider>
+            </Router>
+        );
+    }
 };
 
 App.propTypes = {};
