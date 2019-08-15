b2cf974f64bdbcd940e7581e54940552dffdb0e1
joerg <sendstuff2bo@gmail.com>
2019-08-15 12:12:09 +0930

    disable theme selector, #262 survey info

--- front/src/components/HeaderNav.jsx
+++ front/src/components/HeaderNav.jsx
@@ -20,7 +20,8 @@ const toggle = function(el){
     el.className += ' show';
 };
 
-const HeaderNav = function({ location, surveys, surveyProvider, siteName}) {
+const HeaderNav = function({ location, surveyIds, surveyProvider, siteName}) {
+    
     return (
         <header>
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark shadow-sm">
@@ -39,23 +40,22 @@ const HeaderNav = function({ location, surveys, surveyProvider, siteName}) {
 
                 <div className="collapse navbar-collapse" id="header-nav--collapse">
                     <ul className="navbar-nav mr-auto">
-                    {
-                        surveys.length > 1 &&
-                            <li className={ (/^\/surveys/.test(location.pathname)) ? 'nav-item active' : 'nav-item' }>
-                                <Link className="nav-link" to="/surveys">Survey List</Link>
-                            </li>
-                    }
-                    <li>
-                        <DropdownMenu title="Demos">
-                            <MenuLink to="/demo/form">Form Elements</MenuLink>
-                            <MenuLink to="/demo/analyse">Analysis</MenuLink>
-                            <MenuLink to="/demo/manifest">Manifest</MenuLink>
-                        </DropdownMenu>
-                    </li>
+                        <li className={ (/^\/surveys/.test(location.pathname)) ? 'nav-item active' : 'nav-item' }>
+                            <Link className="nav-link" to="/surveys">Surveys</Link>
+                        </li>
                     </ul>
 
                     <ul className="navbar-nav mt-2 mt-md-0">
-                        <ThemePicker className="nav-item active my-2 my-sm-0" />
+                        <li className="nav-item">
+                            <DropdownMenu title="Demos">
+                                <MenuLink to="/demo/form">Form Elements</MenuLink>
+                                <MenuLink to="/demo/analyse">Analysis</MenuLink>
+                                <MenuLink to="/demo/manifest">Manifest</MenuLink>
+                            </DropdownMenu>
+                        </li>
+                        { /*<li className="nav-item ml-2">
+                            <ThemePicker className="form-control" />
+                        </li> */}
                     </ul>
                 </div>
             </nav>
@@ -63,8 +63,10 @@ const HeaderNav = function({ location, surveys, surveyProvider, siteName}) {
     );
 };
 
+HeaderNav.defaultProps = {
+};
+
 HeaderNav.propTypes = {
-    surveys: PropTypes.arrayOf(PropTypes.string).isRequired,
     surveyProvider: PropTypes.string.isRequired,
     siteName: PropTypes.string.isRequired,
 };
