17a8dfe74b78dbb148eb9190b84e332ce2760a39
joerg <sendstuff2bo@gmail.com>
2019-11-08 14:05:22 +1030

    #294 Update Analysis model, docs

--- front/src/Analysis.js
+++ front/src/Analysis.js
@@ -1,11 +1,89 @@
+/**
+ * @module answer object to backend csv row parser
+ *
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * @typedef EvaluationCondition
+ * @type {object}
+ * @property {string} condition
+ * @property {string} subcondition
+ * @property {string} mainText
+ * @property {string} learnMore
+ * @property {string} mainRecommendation
+ * @property {string} mandatoryTips
+ * @property {string} additionalInsights
+ */
+
+/**
+ * @typedef EvaluationInsightItem
+ * @type {object}
+ * @property {string} displayName
+ * @property {string} displayText
+ */
+
+/**
+ * @typedef EvaluationDisplayResult
+ * @type {object}
+ * @property {EvaluationInsightItem[]} additionalInsights
+ * @property {EvaluationCondition[]} conditions
+ */
+
+/**
+ * @typedef Evaluation
+ * @type {object}
+ * @property {string} category
+ * @property {string} classification
+ * @property {number} rank
+ * @property {string} recommendation
+ * @property {number} riskRating
+ * @property {EvaluationDisplayResult[]} displayResults
+ */
+
+/**
+* Get Proptypes schema
+* @returns {PropTypes}
+*/
+const evaluationPropTypes = function () {
+    return PropTypes.shape({
+        category: PropTypes.string, // TODO check if still used
+        classification: PropTypes.string,
+        rank: PropTypes.number,
+        recommendation: PropTypes.string,
+        riskRating: PropTypes.number,
+        displayResults: PropTypes.shape({
+            additionalInsights: PropTypes.arrayOf(
+                PropTypes.shape({
+                    displayName: PropTypes.string,
+                    displayText: PropTypes.string,
+                })
+            ),
+            conditions: PropTypes.shape({
+                condition: PropTypes.string,
+                subcondition: PropTypes.string,
+                mainText: PropTypes.string,
+                learnMore: PropTypes.string,
+                mainRecommendation: PropTypes.string,
+                mandatoryTips: PropTypes.string,
+                additionalInsights: PropTypes.string,
+            })
+        }),
+
+
+    });
+};
 
 const mockAnalysis = function() {
-    return {
-        category: "Vogonic",
-        classification: "Mild conditions",
+    return [{
+        category: "",
+        classification: "Mild",
         displayResults: {
-            additionalInsights: [],
-            sleepConditions: {
+            additionalInsights: [{
+                displayName: "Avoid Vogons breath",
+                displayText: "Avoid vogons, I that cannot be avoided, turn away in presence of a Vogon."
+            }],
+            conditions: {
                 condition: "Vogonic Flue",
                 subcondition: "Confusion",
                 mainText: "It is known that there are an infinite number of worlds, simply because there is an infinite amount of space for them to be in. \n However, not every one of them is inhabited. Therefore, there must be a finite number of inhabited worlds. \n Any finite number divided by infinity is as near to nothing as makes no odds, so the average population of all the planets in the Universe can be said to be zero. \n From this it follows that the population of the whole Universe is also zero, and that any people you may meet from time to time are merely the products of a deranged imagination.",
@@ -18,24 +96,43 @@ const mockAnalysis = function() {
         rank: 2,
         recommendation: "",
         riskRating: 0
-    };
+    }, {
+        category: "",
+        classification: "Severe",
+        displayResults: {
+            additionalInsights: [],
+            conditions: {
+                condition: "Disorientation",
+                subcondition: "You are lost in space",
+                mainText: "It is known that there are an infinite number of worlds, simply because there is an infinite amount of space for them to be in. \n However, not every one of them is inhabited. Therefore, there must be a finite number of inhabited worlds. \n Any finite number divided by infinity is as near to nothing as makes no odds, so the average population of all the planets in the Universe can be said to be zero. \n From this it follows that the population of the whole Universe is also zero, and that any people you may meet from time to time are merely the products of a deranged imagination.",
+                learnMore: "Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.",
+                mainRecommendation: "Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.",
+                mandatoryTips: "Eskimos had over two hundred different words for snow, without which their conversation would probably have got very monotonous.",
+                additionalInsights: "The Total Perspective Vortex derives its picture of the whole Universe on the principle of extrapolated matter analyses.To explain — since every piece of matter in the Universe is in some way affected by every other piece of matter in the Universe, it is in theory possible to extrapolate the whole of creation — every sun, every planet, their orbits, their composition and their economic and social history from, say, one small piece of fairy cake. The man who invented the Total Perspective Vortex did so basically in order to annoy his wife."
+            }
+        },
+        rank: 2,
+        recommendation: "",
+        riskRating: 0
+    }];
 };
+// console.log(JSON.stringify(mockAnalysis(), null, 4));
 
 /**
  * Sets default values and normalizes an /analyse api response object
  * @param {object} response
  * @returns {object}
  */
-const normalizeAnalysis = function(response) {
+const normalizeEvaluation = function(response) {
     const r = response || {};
 
     return Object.assign({
-        category: 'No category',
-        classification: 'No classification',
+        category: '',
+        classification: '',
         displayResults: {
             additionalInsights: [],
-            sleepConditions: {
-                condition: 'No condition',
+            conditions: {
+                condition: '',
                 subcondition: '',
                 mainText: '',
                 learnMore: '',
@@ -50,4 +147,4 @@ const normalizeAnalysis = function(response) {
     }, r);
 }
 
-export { normalizeAnalysis, mockAnalysis };
+export { mockAnalysis, normalizeEvaluation, evaluationPropTypes };
