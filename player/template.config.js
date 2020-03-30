2ada4c9cbe2a18a6b769c5dbaa29b7661b42a0db
joerg <sendstuff2bo@gmail.com>
2020-03-30 13:47:00 +1030

    #324 example response assertion hooks

--- player/template.config.js
+++ player/template.config.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 /**
  * Copy this file to ./configs/<MY NAME>.js and edit your survey settings
  */
@@ -13,6 +15,15 @@ module.exports = {
         // authentication: http basic or digest
         AUTH: null, // format: 'myuser:mypassword'
         USE_DIGEST: false,
+    },
+
+    //// run assertions on answer responses: { answer => callback(response) }
+    assertions: {
+        q1a: (response) => {
+            const { next_questions } = response;
+            const ids = next_questions.map(q => q.id);
+            assert.ok(ids.indexOf('q1b' ) > -1);
+        },
     },
 
     //// provide custom answers fo given question ids, overwrites the generic question for that question
