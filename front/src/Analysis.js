9ead22170105b1624a32c8d9563942f7ce882f35
joerg <sendstuff2bo@gmail.com>
2020-05-28 16:03:43 +0930

    #352 update Analysis model

--- front/src/Analysis.js
+++ front/src/Analysis.js
@@ -14,7 +14,7 @@ import { isArray } from './Utils';
  */
 
 /**
- * @typedef AdditionalInsight
+ * @typedef Insight
  * @type {object}
  * @property {string} displayName
  * @property {string} displayText
@@ -29,20 +29,13 @@ import { isArray } from './Utils';
  * @property {string} learnMore
  * @property {string} mainRecommendation
  * @property {string} mandatoryTips
- * @property {string} additionalInsights
- */
-
-/**
- * @typedef EvaluationInsightItem
- * @type {object}
- * @property {string} displayName
- * @property {string} displayText
+ * @property {string|Insight[]} additionalInsights, includes legacy string format (#351)
  */
 
 /**
  * @typedef EvaluationDisplayResult
  * @type {object}
- * @property {EvaluationInsightItem[]} additionalInsights
+ * @property {Insight[]} additionalInsights
  * @property {Condition[]} conditions
  */
 
@@ -79,9 +72,9 @@ const Models = {
     },
 
     /**
-     * @returns {AdditionalInsight} additionalInsights
+     * @returns {Insight} additionalInsights
      */
-    additionalInsight: function() {
+    insight: function() {
         return {
             displayName: '',
             displayText: '',
@@ -99,14 +92,14 @@ const Models = {
             learnMore: '',
             mainRecommendation: '',
             mandatoryTips: '',
-            additionalInsights: ''
+            additionalInsights: [], // #351
         };
     },
 
     displayResults: function() {
         return {
             additionalInsights: [],
-            conditions: {},
+            conditions: Models.conditions(),
         };
     },
 
@@ -117,7 +110,7 @@ const Models = {
         return {
             category: '',
             classification: '',
-            displayResults: {},
+            displayResults: Models.displayResults(),
             rank: 0,
             recommendation: '',
             riskRating: 0
@@ -130,12 +123,25 @@ const Models = {
     analysis: function() {
         return {
             version: '',
-            created: 0,
+            created: '',
             constraints: [],
             evaluations: [],
         };
     },
 
+};
+
+/**
+* Get Proptypes schema
+* @returns {PropTypes}
+*
+* #350, add meta data wrapper
+*/
+const insightPropTypes = function () {
+    return PropTypes.shape({
+        displayName: PropTypes.string,
+        displayText: PropTypes.string,
+    });
 };
 
 /**
@@ -151,10 +157,7 @@ const evaluationPropTypes = function () {
         riskRating: PropTypes.number,
         displayResults: PropTypes.shape({
             additionalInsights: PropTypes.arrayOf(
-                PropTypes.shape({
-                    displayName: PropTypes.string,
-                    displayText: PropTypes.string,
-                })
+                insightPropTypes()
             ),
             conditions: PropTypes.shape({
                 condition: PropTypes.string,
@@ -163,7 +166,11 @@ const evaluationPropTypes = function () {
                 learnMore: PropTypes.string,
                 mainRecommendation: PropTypes.string,
                 mandatoryTips: PropTypes.string,
-                additionalInsights: PropTypes.string,
+                // #352, add complex model, allow simple strings
+                additionalInsights: PropTypes.oneOf([
+                    PropTypes.string,
+                    PropTypes.arrayOf(insightPropTypes()),
+                ])
             })
         }),
     });
@@ -189,43 +196,52 @@ const analysisPropTypes = function () {
 
 const mockAnalysis = function() {
     const evaluations =  [{
-        category: "",
-        classification: "Mild",
+        category: '',
+        classification: 'Mild',
         displayResults: {
             additionalInsights: [{
-                displayName: "Avoid Vogons breath",
-                displayText: "Avoid vogons, I that cannot be avoided, turn away in presence of a Vogon."
+                displayName: 'Avoid Vogons breath',
+                displayText: 'Avoid vogons, I that cannot be avoided, turn away in presence of a Vogon.'
             }],
             conditions: {
-                condition: "Vogonic Flue",
-                subcondition: "Confusion",
-                mainText: "It is known that there are an infinite number of worlds, simply because there is an infinite amount of space for them to be in. \n However, not every one of them is inhabited. Therefore, there must be a finite number of inhabited worlds. \n Any finite number divided by infinity is as near to nothing as makes no odds, so the average population of all the planets in the Universe can be said to be zero. \n From this it follows that the population of the whole Universe is also zero, and that any people you may meet from time to time are merely the products of a deranged imagination.",
-                learnMore: "Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.",
-                mainRecommendation: "Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.",
-                mandatoryTips: "Eskimos had over two hundred different words for snow, without which their conversation would probably have got very monotonous.",
-                additionalInsights: "The Total Perspective Vortex derives its picture of the whole Universe on the principle of extrapolated matter analyses.To explain — since every piece of matter in the Universe is in some way affected by every other piece of matter in the Universe, it is in theory possible to extrapolate the whole of creation — every sun, every planet, their orbits, their composition and their economic and social history from, say, one small piece of fairy cake. The man who invented the Total Perspective Vortex did so basically in order to annoy his wife."
+                condition: 'Vogonic Flue',
+                subcondition: 'Confusion',
+                mainText: 'It is known that there are an infinite number of worlds, simply because there is an infinite amount of space for them to be in. \n However, not every one of them is inhabited. Therefore, there must be a finite number of inhabited worlds. \n Any finite number divided by infinity is as near to nothing as makes no odds, so the average population of all the planets in the Universe can be said to be zero. \n From this it follows that the population of the whole Universe is also zero, and that any people you may meet from time to time are merely the products of a deranged imagination.',
+                learnMore: 'Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.',
+                mainRecommendation: 'Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.',
+                mandatoryTips: 'Eskimos had over two hundred different words for snow, without which their conversation would probably have got very monotonous.',
+                additionalInsights: [
+                    // #352, legacy format
+                    'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+                    // #352, insight format
+                    {
+                        displayName: 'Don\'t panic',
+                        displayText: 'There is a theory which states that if ever anyone discovers exactly what the Universe is for and why it is here, it will instantly disappear and be replaced by something even more bizarre and inexplicable. There is another theory which states that this has already happened.'
+                    }
+                ]
             }
         },
         rank: 2,
-        recommendation: "",
+        recommendation: '',
         riskRating: 0
     }, {
-        category: "",
-        classification: "Severe",
+        category: '',
+        classification: 'Severe',
         displayResults: {
             additionalInsights: [],
             conditions: {
-                condition: "Disorientation",
-                subcondition: "You are lost in space",
-                mainText: "It is known that there are an infinite number of worlds, simply because there is an infinite amount of space for them to be in. \n However, not every one of them is inhabited. Therefore, there must be a finite number of inhabited worlds. \n Any finite number divided by infinity is as near to nothing as makes no odds, so the average population of all the planets in the Universe can be said to be zero. \n From this it follows that the population of the whole Universe is also zero, and that any people you may meet from time to time are merely the products of a deranged imagination.",
-                learnMore: "Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.",
-                mainRecommendation: "Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.",
-                mandatoryTips: "Eskimos had over two hundred different words for snow, without which their conversation would probably have got very monotonous.",
-                additionalInsights: "The Total Perspective Vortex derives its picture of the whole Universe on the principle of extrapolated matter analyses.To explain — since every piece of matter in the Universe is in some way affected by every other piece of matter in the Universe, it is in theory possible to extrapolate the whole of creation — every sun, every planet, their orbits, their composition and their economic and social history from, say, one small piece of fairy cake. The man who invented the Total Perspective Vortex did so basically in order to annoy his wife."
+                condition: 'Disorientation',
+                subcondition: 'You are lost in space',
+                mainText: 'It is known that there are an infinite number of worlds, simply because there is an infinite amount of space for them to be in. \n However, not every one of them is inhabited. Therefore, there must be a finite number of inhabited worlds. \n Any finite number divided by infinity is as near to nothing as makes no odds, so the average population of all the planets in the Universe can be said to be zero. \n From this it follows that the population of the whole Universe is also zero, and that any people you may meet from time to time are merely the products of a deranged imagination.',
+                learnMore: 'Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.',
+                mainRecommendation: 'Make it totally clear that this gun has a right end and a wrong end. Make it totally clear to anyone standing at the wrong end that things are going badly for them.',
+                mandatoryTips: 'Eskimos had over two hundred different words for snow, without which their conversation would probably have got very monotonous.',
+                // #352, legacy format
+                additionalInsights: 'The Total Perspective Vortex derives its picture of the whole Universe on the principle of extrapolated matter analyses.To explain — since every piece of matter in the Universe is in some way affected by every other piece of matter in the Universe, it is in theory possible to extrapolate the whole of creation — every sun, every planet, their orbits, their composition and their economic and social history from, say, one small piece of fairy cake. The man who invented the Total Perspective Vortex did so basically in order to annoy his wife.'
             }
         },
         rank: 2,
-        recommendation: "",
+        recommendation: '',
         riskRating: 0
     }];
 
@@ -240,6 +256,24 @@ const mockAnalysis = function() {
     }
 };
 // console.log(JSON.stringify(mockAnalysis(), null, 4));
+
+
+/**
+ * Sets default values and normalizes an /analyse api response object
+ * @param {object} response
+ * @returns {object}
+ *
+ * #352, legacy support allow 'string', 'string[]'
+ */
+const normalizeInsight = function(insight) {
+    let r = insight || {}
+    if (typeof r === 'string') {
+        r  = {
+            displayText: r
+        };
+    }
+    return Object.assign(Models.insight(), r);
+};
 
 /**
  * Sets default values and normalizes an /analyse api response object
@@ -256,7 +290,12 @@ const normalizeEvaluation = function(response) {
     const a = r.displayResults.additionalInsights;
     const c = r.displayResults.conditions;
 
-    r.displayResults.additionalInsights = a.map(item => Object.assign(Models.additionalInsight(), item));
+    // #352
+    let ci = c.additionalInsights || [];
+    ci = (typeof ci == 'string') ? [ci] : ci;
+    c.additionalInsights = ci.map(item => normalizeInsight(item))
+
+    r.displayResults.additionalInsights = a.map(item => normalizeInsight(item));
     r.displayResults.conditions = Object.assign(Models.conditions(), c);
     return r;
 }
@@ -289,4 +328,13 @@ const normalizeAnalysis = function(response) {
     return r;
 }
 
-export { Models, mockAnalysis, normalizeEvaluation, normalizeAnalysis, evaluationPropTypes, analysisPropTypes };
+export {
+    Models,
+    mockAnalysis,
+    normalizeInsight,
+    normalizeEvaluation,
+    normalizeAnalysis,
+    insightPropTypes,
+    evaluationPropTypes,
+    analysisPropTypes
+};
