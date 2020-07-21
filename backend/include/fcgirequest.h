2c3bfcad39dc7ef716cbd75c9a4c0eb25f3b8a69
joerg <sendstuff2bo@gmail.com>
2020-07-21 10:24:13 +0930

    #363 add, rename function declarations, cleanup

--- backend/include/fcgirequest.h
+++ backend/include/fcgirequest.h
@@ -7,15 +7,8 @@
 #define X_HEADER_MW_USER "X-SurveyProxy-Auth-User"
 #define X_HEADER_MW_GROUP "X-SurveyProxy-Auth-Group"
 
-int parse_session_meta_kreq(struct kreq *req, struct session_meta *meta);
-enum Authorized {
-  MWAUTHORITY_NOT_DEFINED,
-  MWAUTHORITY_INVALID,
-  MWAUTHORITY_INVALID_XHEADER,
-  MWAUTHORITY_NO_AUTHENTICATION,
-  MW_AUTHORITY_OK,
-};
-enum khttp validate_session_meta_kreq(struct kreq *req, struct session_meta *meta);
-
+struct session_meta *fcgirequest_parse_session_meta(struct kreq *req);
+enum khttp fcgirequest_validate_request(struct kreq *req, struct session_meta *meta);
+enum khttp fcgirequest_validate_session_request(struct kreq *req, struct session *ses);
 #endif
 
