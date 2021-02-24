3e77c1f46dd044dea82864572f77da8b30d633bb
joerg <sendstuff2bo@gmail.com>
2021-02-25 10:24:37 +1030

    #407, return error on delete nonexistant qnswer

--- backend/src/sessions.c
+++ backend/src/sessions.c
@@ -1609,7 +1609,7 @@ int session_add_answer(struct session *ses, struct answer *a) {
  * It is not an error if there were no matching answers to delete
  * #407, #13, improve counting (only answers where deletion flag changed), make use of helper functions
  *
- * returns number of deleted answers or -1 on (arg validation) error. If there is no exactly matching answer, it will return 0.
+ * returns number of deleted answers or -1 on (arg validation) error. If there is no exactly matching answer, it will return an error.
 */
 int session_delete_answers_by_question_uid(struct session *ses, char *uid, int deleteFollowingP) {
   int retVal = 0;
@@ -1625,8 +1625,7 @@ int session_delete_answers_by_question_uid(struct session *ses, char *uid, int d
 
     int index = session_get_answer_index(uid, ses);
     if (index < 0) {
-      LOG_WARNV("Delete Answer (by uid): answer '%s' not found in (session '%s')", uid, ses->session_id);
-      break; // leave with 0
+      LOG_ERRORV("Delete Answer (by uid): answer '%s' not found in (session '%s')", uid, ses->session_id);
     }
 
     // #363, system answers (QTYPE_META or @uid) answers cannot be deleted
