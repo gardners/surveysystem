0d05e146831cf1d51a20501a8a10e037b81c9d73
joerg <sendstuff2bo@gmail.com>
2021-07-07 14:50:03 +0930

    #561, add fn for splitting by sep

--- backend/include/utils.h
+++ backend/include/utils.h
@@ -6,4 +6,6 @@
 void freez(void *p);
 void trim_crlf(char *line);
 struct tm *format_time_ISO8601(time_t t, char *buf, size_t len);
+
+char *parse_line(const char *body, char separator, char **saveptr); // #461
 #endif
