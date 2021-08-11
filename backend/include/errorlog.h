67213eefb834b34acb018454bb8e03c6cfc5b2f4
joerg <sendstuff2bo@gmail.com>
2021-08-11 13:23:34 +0930

    add macro for logging custom error codes

--- backend/include/errorlog.h
+++ backend/include/errorlog.h
@@ -1,6 +1,12 @@
 #ifndef __ERRORLOG_H__
 #define __ERRORLOG_H__
 
+enum ss_err {
+  SS_ERROR = -1,
+  SS_OK = 0,
+};
+const char *ss_strerr(enum ss_err); // errorlog.c
+
 void clear_errors(void);
 void dump_errors(FILE *F);
 int remember_error(const char *severity, const char *file, const int line,const char *function, const char *message, ...);
@@ -10,27 +16,36 @@ void code_instrumentation_unmute();
 #define LOG_MUTE() code_instrumentation_mute()
 #define LOG_UNMUTE() code_instrumentation_unmute()
 
-#define BREAK_ERRORV(MSG, ...)                                                 \
-  {                                                                            \
-    retVal = -1;                                                               \
-    remember_error("ERROR", __FILE__, __LINE__, __FUNCTION__, MSG,             \
-                   __VA_ARGS__);                                               \
-    break;                                                                     \
-  }
-#define LOG_WARNV(MSG, ...)                                                    \
-  {                                                                            \
-    remember_error("WARNING", __FILE__, __LINE__, __FUNCTION__, MSG,           \
-                   __VA_ARGS__);                                               \
-  }
-#define BREAK_ERROR(MSG)                                                       \
-  {                                                                            \
-    retVal = -1;                                                               \
-    remember_error("ERROR", __FILE__, __LINE__, __FUNCTION__, "%s", MSG);      \
-    break;                                                                     \
-  }
-
 #define LOG_INFOV(MSG, ...) log_message(__FILE__, __FUNCTION__, __LINE__, MSG, __VA_ARGS__)
 #define LOG_INFO(MSG) log_message(__FILE__, __FUNCTION__, __LINE__, "%s", MSG)
+
+#define LOG_WARNV(MSG, ...) {                                                              \
+    remember_error("WARN", __FILE__, __LINE__, __FUNCTION__, MSG, __VA_ARGS__);            \
+  }
+
+#define BREAK_ERRORV(MSG, ...) {                                                           \
+    retVal = SS_ERROR;                                                                     \
+    remember_error(ss_strerr(retVal), __FILE__, __LINE__, __FUNCTION__, MSG, __VA_ARGS__); \
+    break;                                                                                 \
+  }
+
+#define BREAK_ERROR(MSG) {                                                                 \
+    retVal = SS_ERROR;                                                                     \
+    remember_error(ss_strerr(retVal), __FILE__, __LINE__, __FUNCTION__, "%s", MSG);        \
+    break;                                                                                 \
+  }
+
+#define BREAK_CODEV(CODE, MSG, ...) {                                                      \
+    retVal = CODE;                                                                         \
+    remember_error(ss_strerr(retVal), __FILE__, __LINE__, __FUNCTION__, MSG, __VA_ARGS__); \
+    break;                                                                                 \
+  }
+
+#define BREAK_CODE(CODE, MSG) {                                                            \
+    retVal = CODE;                                                                         \
+    remember_error(ss_strerr(retVal), __FILE__, __LINE__, __FUNCTION__, "%s", MSG);        \
+    break;                                                                                 \
+  }
 
 #define CHECKPOINT() fprintf(stderr, "%s:%d:%s():pid=%d: Checkpoint\n", __FILE__, __LINE__, __FUNCTION__, getpid())
 
