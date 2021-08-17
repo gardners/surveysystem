b8fdda1717f9152681ac22e678b76d66ceea8211
joerg <sendstuff2bo@gmail.com>
2021-08-17 15:28:39 +0930

    on #260, #414 implement endpoints DELETE /answers, GET/POST /sessions, implement error codes

--- backend/src/fcgi_main.c
+++ backend/src/fcgi_main.c
@@ -28,13 +28,14 @@ static const struct kvalid keys[KEY__MAX] = {
   { kvalid_stringne, "surveyid" },
   { kvalid_stringne, "sessionid" },
   { kvalid_stringne, "questionid" },
-  { kvalid_stringne, "answer" }
+  { kvalid_stringne, "answer" },
+  { kvalid_stringne, "if-match" }
 };
 
 typedef void (*disp)(struct kreq *);
 
 static void fcgi_index(struct kreq *);
-static void fcgi_newsession(struct kreq *);
+static void fcgi_page_session(struct kreq *);
 static void fcgi_page_answers(struct kreq *);
 static void fcgi_addanswer(struct kreq *);
 static void fcgi_updateanswer(struct kreq *);
@@ -50,7 +51,7 @@ static enum khttp sanitise_page_request(const struct kreq *req);
 
 static const disp disps[PAGE__MAX] = {
     fcgi_index,
-    fcgi_newsession,
+    fcgi_page_session,
     fcgi_page_answers,
     fcgi_addanswer,
     fcgi_updateanswer,
@@ -65,7 +66,7 @@ static const disp disps[PAGE__MAX] = {
 
 static const char *const pages[PAGE__MAX] = {
     "index",
-    "newsession",
+    "session",
     "answers",
     "addanswer",
     "updateanswer",
@@ -213,114 +214,225 @@ static void fcgi_index(struct kreq *req) {
   return;
 }
 
-static void fcgi_newsession(struct kreq *req) {
+/**
+ * Parse request payload and add  desrerialised answers to session
+ * #260
+ * PARSE_ANSWER_SERIALISED,
+ * PARSE_ANSWER_VALUES,
+ */
+int fcgi_session_add_answers(struct kreq *req, struct session *ses, int *affected_count) {
   int retVal = 0;
-
-  struct session_meta *meta = NULL;
-  struct session *ses = NULL;
-  struct nextquestions *nq = NULL;
-  enum actions action = ACTION_SESSION_NEW;
+  struct answer_list *list = NULL;
 
   do {
+    BREAK_IF(req == NULL, SS_ERROR_ARG, "req");
+    BREAK_IF(ses == NULL, SS_ERROR_ARG, "ses");
 
-    LOG_INFOV("Entering page handler: '%s' '%s'", kmethods[req->method], req->fullpath);
+    *affected_count =0;
+    int res;
 
-    char *survey_id = fcgi_request_get_field_value(KEY_SURVEYID, req);
-    char *session_id = fcgi_request_get_field_value(KEY_SESSIONID, req); // POST only!
+    // parse answers
 
-    char created_sessid[40] = { '\0' };
-
-    if (validate_survey_id(survey_id)) {
-      http_json_error(req, KHTTP_400, "Surveyid is invalid.");
-      BREAK_ERROR("Invalid survey id");
+    list = fcgi_request_parse_answers_serialised(req, ses, &res);
+    if (res) {
+      BREAK_CODE(res, "error parsing answers (serialised)");
     }
 
-    // #363 parse session meta
-    meta = fcgi_request_parse_meta(req);
-    if (!meta) {
-      http_json_error(req, KHTTP_500, "Session could not be created (parse request meta).");
-      BREAK_ERROR("fcgi_request_parse_meta() failed");
-    }
-
-    // #363 validate session meta
-    enum khttp status = fcgi_request_validate_meta_kreq(req, meta);
-    if (status >= KHTTP_400) {
-      http_json_error(req, status, "Invalid idendity provider check app configuration");
-      BREAK_ERRORV("fcgi_request_validate_meta_kreq() returned status %d >= KHTTP_400 (%d)", KHTTP_400, status);
-    }
-
-    if (KMETHOD_POST == req->method) {
-
-      if (meta->provider != IDENDITY_HTTP_TRUSTED) {
-        // only allowed for trusted middleware, so return 400
-        http_json_error(req, KHTTP_400, "POST session_id only allowed in a managed system.");
-        BREAK_ERRORV("POST /newsession with invalid provider %d", meta->provider);
+    if (!list) {
+      list = fcgi_request_parse_answers_values(req, ses, &res);
+      if (res) {
+        BREAK_CODE(res, "error parsing answers (values)");
       }
-
-      if (validate_session_id(session_id)) {
-        // valid uid or return 400
-        http_json_error(req, KHTTP_400, "Invalid session_id");
-        BREAK_ERROR("Invalid session_id");
-      }
-
-      char sess_path[1024];
-      if(generate_session_path(session_id, session_id, sess_path, 1024)) {
-        http_json_error(req, KHTTP_500, "Failed to create session path");
-        BREAK_ERROR("Failed to create session path");
-      }
-
-      if(!access(sess_path, F_OK)) {
-        // valid uid or return 400
-        http_json_error(req, KHTTP_400, "Session exists already");
-        BREAK_ERRORV("Session '%s' exists already", session_id);
-      }
-
-    } else {
-
-      // #239, create new session id (separated out)
-      if(create_session_id(created_sessid, 40)) {
-        http_json_error(req, KHTTP_500, "Session could not be created (create session id).");
-        BREAK_ERROR("create session id failed");
-      }
-      session_id = created_sessid;
-
-    } // if POST
-
-    // #363 save session meta into session
-    // #268 get ses->consistency_sha
-    ses = create_session(survey_id, session_id, meta);
-    if (!ses) {
-      http_json_error(req, KHTTP_500, "Session could not be created (create session).");
-      BREAK_ERROR("create_session() failed");
     }
 
-    // # 461 store nextquestions after creating session
-    nq = get_next_questions(ses, action, 0);
-    if (!nq) {
-      http_json_error(req, KHTTP_500, "Unable to get next questions.");
-      BREAK_ERROR("get_next_questions() failed");
+    if (!list) {
+      BREAK_CODE(SS_MISSING_ANSWER, "Could find find answers in request");
     }
 
-    if (save_session(ses)) {
-      http_json_error(req, KHTTP_500, "Unable to update session.");
-      BREAK_ERROR("save_session() failed");
+    // add answers
+    for (int i = 0; i < list->len; i++) {
+      res = validate_session_add_answer(ses, list->answers[i]);
+      BREAK_IF(res != SS_OK, res, NULL);
+
+      // #445 count affected answers
+      *affected_count += session_add_answer(ses, list->answers[i]);
+      if (*affected_count < 0) {
+        BREAK_CODEV(SS_INVALID_ANSWER, "Failed to add answer", list->answers[i]->uid);
+      }
     }
 
-    // reply
-    http_open(req, KHTTP_200, KMIME_TEXT_PLAIN, ses->consistency_hash);
-    enum kcgi_err er = khttp_puts(req, session_id);
-    if (er != KCGI_OK) {
-      BREAK_ERROR("khttp_puts() failed");
+    if (retVal) {
+      break;
     }
-
-    LOG_INFO("Leaving page handler");
 
   } while (0);
 
+  free_answer_list(list);
+  return retVal;
+}
+
+int fcgi_session_delete_answers(struct kreq *req, struct session *ses, int *affected_count) {
+  int retVal = 0;
+
+  do {
+    BREAK_IF(req == NULL, SS_ERROR_ARG, "req");
+    BREAK_IF(ses == NULL, SS_ERROR_ARG, "ses");
+
+    *affected_count = 0;
+    int res;
+
+    // validate If-Match (consistency sha)
+    char *sha1 = fcgi_request_get_consistency_hash(req);
+    if (!sha1) {
+      BREAK_CODE(SS_INVALID_CONSISTENCY_HASH, "missing if-match header/param");
+    }
+
+    if (sha1_validate_string_hashlike(sha1)) {
+      BREAK_CODE(SS_INVALID_CONSISTENCY_HASH, "invalid if-match header/param");
+    }
+
+    // determine parser
+    int del_prev = 0;
+    char *uid = fcgi_request_get_field_value(KEY_QUESTION_ID, req);
+
+    if (!uid) {
+      del_prev = 1;
+      struct answer *last = session_get_last_given_answer(ses);
+      uid = (last) ? last->uid : NULL;
+
+      if(!uid) {
+        BREAK_CODE(SS_OK, "(pass) deletion previous answer on an empty or fully deleted session");
+      }
+    }
+
+    LOG_INFOV("preparing to delete answers: mode: '%s', uid: '%s'", (del_prev) ? "previous" : "until", uid);
+
+    res = validate_session_delete_answer(ses, uid);
+    BREAK_IF(res != SS_OK, res, NULL);
+
+    *affected_count = session_delete_answer(ses, uid);
+    if (affected_count < 0) {
+      // TODO could be both 400 or 500 (storage, serialization, not in session)
+      BREAK_CODEV(SS_INVALID_ANSWER, "Failed to add answer", uid);
+    }
+
+  } while (0);
+
+  return retVal;
+}
+
+/**
+ * #260, #461
+ * POST /answers: add answer(s)
+ * HEAD /answers prevalidate adding answer(s)
+ */
+static void fcgi_page_answers(struct kreq *req) {
+  int retVal = 0;
+
+  struct session *ses = NULL;
+  struct answer *ans = NULL;
+  struct nextquestions *nq = NULL;
+
+  enum actions action;
+  int affected_count = 0;
+  int res;
+
+  do {
+    LOG_INFOV("Entering page handler: '%s' '%s'", kmethods[req->method], req->fullpath);
+    BREAK_IF(req == NULL, SS_ERROR_ARG, "req");
+
+    // validate allowed methods #260, #461
+    // ACTION__NONE indicates undefined action
+    switch (req->method) {
+      case KMETHOD_HEAD:
+      case KMETHOD_PUT:
+      case KMETHOD_POST:
+        action = ACTION_SESSION_ADDANSWER;
+      break;
+
+      case KMETHOD_DELETE:
+        action = ACTION_SESSION_DELETEANSWER;
+      break;
+
+      default:
+        action = ACTION_NONE;
+    }
+
+    LOG_INFOV("action: '%s'", session_action_names[action]);
+
+    if (action == ACTION_NONE) {
+      BREAK_CODE(SS_INVALID_METHOD, NULL);
+    }
+
+    ses = fcgi_request_load_and_verify_session(req, action, &res);
+    if (!ses) {
+      BREAK_CODE(res, "failed to load session");
+    }
+
+    // dispatcher:
+    //    POST   /answers?sessionid=<session_id>&answer=q1:a1:0:0:0:0:0:0:0
+    //    POST   /answers?sessionid=<session_id> -d "q1:a1:0:0:0:0:0:0:0\nq2:a2:0:0:0:0:0:0:0"
+    //    POST   /answers?sessionid=<session_id>&q1=a1
+    //    POST   /answers?sessionid=<session_id>&q1=a1&q2=a1
+    //    DELETE /answers?sessionid=<session_id>&uid=q1&sha=<consistency_sha>
+    //    DELETE /answers?sessionid=<session_id>&sha=<consistency_sha> (previous anwers)
+
+    switch (action) {
+      case ACTION_SESSION_ADDANSWER:
+        res = fcgi_session_add_answers(req, ses, &affected_count);
+      break;
+
+      case ACTION_SESSION_DELETEANSWER:
+        res = fcgi_session_delete_answers(req, ses, &affected_count);
+      break;
+
+      default:
+        res = SS_INVALID_ACTION;
+    }
+
+    if (res) {
+      BREAK_CODEV(res, "failed to handle action '%s'", session_action_names[action]);
+    }
+
+    // #332 next_questions data struct
+    nq = get_next_questions(ses, action, affected_count);
+    if (!nq) {
+      BREAK_CODE(SS_SYSTEM_GET_NEXTQUESTIONS, "failed to get next questions'");
+    }
+
+    // #494  HEAD request: do not create and exit
+    if (req->method == KMETHOD_HEAD) {
+      http_open(req, KHTTP_200, KMIME_APP_JSON, ses->consistency_hash);
+      khttp_puts(req, NULL);
+      LOG_INFO("Leaving page handler.");
+      break;
+    }
+
+    // joerg: break if session could not be updated
+    res = save_session(ses);
+    if (res) {
+      BREAK_CODE(res, "save_session failed");
+    }
+
+    // All ok, so tell the caller the next question to be answered
+    // #373, separate response handler for nextquestions
+    res = fcgi_response_nextquestion(req, ses, nq);
+    if (res) {
+      BREAK_CODE(res, "Could write page response (next questions)");
+    }
+
+    LOG_INFO("Leaving page handler.");
+  } while (0);
+
   // destruct
-  free_session_meta(meta);
   free_session(ses);
+  free_answer(ans);
   free_next_questions(nq);
+
+  // error response
+  if (retVal) {
+    fcgi_error_response(req, retVal);
+  }
 
   (void)retVal;
   return;
@@ -331,115 +443,87 @@ static void fcgi_newsession(struct kreq *req) {
  * POST /answers: add answer(s)
  * HEAD /answers prevalidate adding answer(s)
  */
-static void fcgi_page_answers(struct kreq *req) {
+static void fcgi_page_session(struct kreq *req) {
   int retVal = 0;
-  enum khttp status;
 
+  struct session_meta *meta = NULL;
   struct session *ses = NULL;
-  struct answer *ans = NULL;
-  struct nextquestions *nq = NULL;
-  char *uids = NULL;
 
-  enum actions action = ACTION_SESSION_ADDANSWER;
-  enum kmethod methods[3] = { KMETHOD_HEAD, KMETHOD_POST, KMETHOD_PUT };
+  enum actions action;
+  char session_id[40];
+
+  char *survey_id = NULL;
+  char *param = NULL;
+  int res;
 
   do {
-
     LOG_INFOV("Entering page handler: '%s' '%s'", kmethods[req->method], req->fullpath);
 
+    BREAK_IF(req == NULL, SS_ERROR_ARG, "req");
+
     // validate allowed methods #260, #461
-    status = fcgi_request_validate_method(req, methods, 3);
-    if (status != KHTTP_200) {
-      http_open(req, status, KMIME_TEXT_PLAIN, NULL);
-      BREAK_ERRORV("validating request method (KMETHOD: %d) failed, status %d != KHTTP_200", req->method, KHTTP_200, status);
+    // ACTION__NONE indicates undefined action
+    switch (req->method) {
+      case KMETHOD_HEAD:
+      case KMETHOD_GET:
+      case KMETHOD_POST:
+        action = ACTION_SESSION_NEW;
+      break;
+
+      default:
+        action = ACTION_NONE;
     }
 
-    ses = fcgi_request_load_session(req);
-    if (!ses) {
-      http_json_error(req, KHTTP_400, "Could not load specified session. Does it exist?");
-      BREAK_ERROR("Could not load session");
+    LOG_INFOV("action: '%s'", session_action_names[action]);
+
+    if (action == ACTION_NONE) {
+      BREAK_CODE(SS_INVALID_METHOD, NULL);
     }
 
-    /*
-     FILE *fp = open_log("session.log");
-     dump_session(fp, ses);
-    */
-
-    // validate request against session meta (#363)
-    status = fcgi_request_validate_meta_session(req, ses);
-    if (status != KHTTP_200) {
-      http_json_error(req, status, "Invalid idendity provider, check app configuration");
-      BREAK_ERRORV("validate_session_meta() returned status %d != (%d)", KHTTP_200, status);
+    // survey id
+    survey_id = fcgi_request_get_field_value(KEY_SURVEY_ID, req);
+    if (validate_survey_id(survey_id)) {
+      BREAK_CODEV(SS_INVALID_SURVEY_ID, "survey: '%s'", (survey_id) ? survey_id : "(null)");
     }
 
-    // validate requested action against session current state (#379)
-    char reason[1024];
-    if (validate_session_action(action, ses, reason, 1024)) {
-      http_json_error(req, KHTTP_400, reason);
-      BREAK_ERROR("Session action validation failed");
+    // request meta(#363)
+    meta = fcgi_request_parse_meta(req);
+    if (!meta) {
+      BREAK_CODEV(SS_SYSTEM_LOAD_SESSION_META, "session: '%s'", session_id);
+    }
+    res = fcgi_request_validate_session_idendity(req, meta);
+    if (res) {
+      BREAK_CODEV(res, "session: '%s'", session_id);
     }
 
-    // see kcgi/child.c fn kworker_child_body()
-    char *value = NULL;
-    value = fcgi_request_get_field_value(KEY_ANSWER, req);
-    if (!value) {
-      LOG_INFO("No key 'answer' found, checking for anonymous data body");
-      value = fcgi_request_get_field_value(KEY__MAX, req);
-    }
+    // session_id
+    switch (req->method) {
+      // create new session id
+      case KMETHOD_GET:
+        if (create_session_id(session_id, 40)) {
+          BREAK_CODE(SS_ERROR, "Unable to create session id");
+        }
+      break;
 
-    if(!value) {
-      BREAK_ERROR("Could find answers value (empty)");
-    }
+      // fetch param session_id
+      case KMETHOD_POST:
+        param = fcgi_request_get_field_value(KEY_SESSION_ID, req);
+        if (validate_session_id(param)) {
+          BREAK_CODEV(SS_INVALID_SESSION_ID, "session: '%s'", (param) ? param : "(null)");
+        }
+        strncpy(session_id, param, 40);
+      break;
 
-    // load answers from request
-    struct answer_list *list = deserialise_answers(value, ANSWER_SCOPE_PUBLIC);
-    if (!list) {
-      http_json_error(req, KHTTP_400, "Could not load answers");
-      BREAK_ERROR("Could not load answers");
-    }
-
-    for (size_t i = 0; i < list->len; i++) {
-      uids = serialise_list_append_alloc(uids, list->answers[i]->uid, ',');
-    }
-
-    int match = strcmp(
-      (uids) ? uids : "<null answers>",
-      (ses->next_questions) ? ses->next_questions : "<null next_questions>"
-    );
-
-    if (match != 0) {
-      http_json_error(req, KHTTP_400, "Answers don't match previous questions (extact order). Retrieve required questions by querying /nextquestions");
-      BREAK_ERRORV("Answers don't match next_questions: '%.50s' != '%.50s'", uids, ses->next_questions);
-    }
-
-    int affected_count = 0;
-    int error = 0;
-
-    for (int i = 0; i < list->len; i++) {
-      if (validate_session_add_answer(ses, list->answers[i])) {
-        error = 1;
-        http_json_error(req, KHTTP_400, "Answer is invalid");
-        BREAK_ERROR("Answer validation failed");
-      }
-
-      if (req->method == KMETHOD_HEAD) {
-        continue;
-      }
-
-      // #445 count affected answers
-      affected_count += session_add_answer(ses, list->answers[i]);
-      if (affected_count < 0) {
-        error = 1;
-        http_json_error(req, KHTTP_400, "Invalid answer, could not add to session.");
-        BREAK_ERROR("session_add_answer() failed.");
-      }
-    } // end for
-
-    if (error) {
+      // (HEAD) nothing
+      default:
       break;
     }
 
-    // HEAD: do not save and exit with empty json body
+    if (retVal) {
+      break;
+    }
+
+    // #494  HEAD request: do not create and exit
     if (req->method == KMETHOD_HEAD) {
       http_open(req, KHTTP_200, KMIME_APP_JSON, ses->consistency_hash);
       khttp_puts(req, NULL);
@@ -447,39 +531,38 @@ static void fcgi_page_answers(struct kreq *req) {
       break;
     }
 
-    // #332 next_questions data struct
-    nq = get_next_questions(ses, action, affected_count);
-    if (!nq) {
-      http_json_error(req, KHTTP_500, "Unable to get next questions.");
-      BREAK_ERROR("get_next_questions() failed");
+    // create session (also adds and stores next_questions)
+    ses = create_session(survey_id, session_id, meta, &res);
+    if (!ses) {
+      BREAK_CODE(res, "failed to create session");
     }
 
-    // joerg: break if session could not be updated
-    if (save_session(ses)) {
-      http_json_error(req, KHTTP_500, "Unable to update session.");
-      BREAK_ERROR("save_session failed()");
+    // response
+    if (http_open(req, KHTTP_200, KMIME_TEXT_PLAIN, ses->consistency_hash)) {
+      BREAK_ERROR("http_open(): unable to initialise http response");
     }
 
-    // All ok, so tell the caller the next question to be answered
-    // #373, separate response handler for nextquestions
-    if (fcgi_response_nextquestion(req, ses, nq)) {
-      http_json_error(req, KHTTP_500, "Could not load next questions for specified session.");
-      BREAK_ERROR("Could not load next questions for specified session");
+    enum kcgi_err er = khttp_puts(req, session_id);
+    if (er != KCGI_OK) {
+      BREAK_ERROR("khttp_puts() failed");
     }
 
     LOG_INFO("Leaving page handler.");
-
   } while (0);
 
   // destruct
-  freez(uids);
+  free_session_meta(meta);
   free_session(ses);
-  free_answer(ans);
-  free_next_questions(nq);
 
+  if (retVal) {
+    fcgi_error_response(req, retVal);
+  }
+
+  // error response
   (void)retVal;
   return;
 }
+
 
 static void fcgi_addanswer(struct kreq *req) {
   int retVal = 0;
@@ -670,7 +753,7 @@ static void fcgi_delanswer(struct kreq *req) {
 
     LOG_INFOV("Entering page handler: '%s' '%s'", kmethods[req->method], req->fullpath);
 
-    char *question_id = fcgi_request_get_field_value(KEY_QUESTIONID, req);
+    char *question_id = fcgi_request_get_field_value(KEY_QUESTION_ID, req);
 
     ses = fcgi_request_load_session(req);
     if (!ses) {
@@ -678,7 +761,7 @@ static void fcgi_delanswer(struct kreq *req) {
       BREAK_ERROR("Could not load session");
     }
 
-    if (validate_session_delete_answer(question_id, ses)) {
+    if (validate_session_delete_answer(ses, question_id)) {
       http_json_error(req, KHTTP_400, "Could load answer. Does it exist?");
       BREAK_ERRORV("Could not load answer '%s'", question_id);
     }
