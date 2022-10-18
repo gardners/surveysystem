1efcbd40c6ce2c18c4cf9eb75deb17d5bb3f136f
joerg <sendstuff2bo@gmail.com>
2022-10-18 10:39:19 +1030

    #401 tests for jwt auth

--- front/src/__tests__/JwtAuth.test.js
+++ front/src/__tests__/JwtAuth.test.js
@@ -1,24 +1,177 @@
+import LocalStorage from '../storage/LocalStorage';
 import Auth from '../JwtAuth';
 
-const access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjU5OTE0OTk5LCJpYXQiOjE2NTk5MTQ2OTksImp0aSI6ImVkNmI3ZGM3ZGJhODQ5YjBhZDk1YmQyZWUwNTYzOWM5IiwidXNlcl9pZCI6MX0.q6yeziwl6AZQiFXMVY8M2VbyX-V8Zx5A8UuwXaftRzU';
 
-describe('JWT Auth', () => {
+const MOCK_ENDPOINT = 'https://test.me';
+const ONE_HOUR = 3600; //seconds
 
-    test('parse_token', () => {
-        let res;
+// store original fetch api
+const _cached_fetch = window.fetch;
 
-        res = Auth.parse_token();
-        expect(res).toBe(null);
+const mockFetchApi = function(resource, options={}) {
+    let data = null;
 
-        res = Auth.parse_token('');
-        expect(res).toBe(null);
+    switch (resource) {
+        case `${MOCK_ENDPOINT}/token`:
+        case `${MOCK_ENDPOINT}/token/refresh`:
+            data = {
+                access:  LocalStorage.get('access'),
+                refresh: LocalStorage.get('refresh'),
+            };
+        break;
+        case `${MOCK_ENDPOINT}/user`:
+            data = {
+                username: LocalStorage.get('mock_username'),
+            };
+        break;
+        default:
+            // nothing
 
-        res = Auth.parse_token('a.a');
-        expect(res).toBe(null);
+    }
 
-        res = Auth.parse_token(access_token);
-        expect(res.token_type).toBe('access');
-        expect(res.user_id).toBe(1);
+    return  Promise.resolve({
+        status: 200,
+        ok: true,
+        text: () => Promise.resolve(JSON.stringify(data)),
+        json: () => Promise.resolve(data),
+    });
+};
+
+
+const timestamp = function(sec=0) {
+    const now  = Math.floor(Date.now()/1000);
+    return now + sec;
+}
+
+const create_jwt_token = function(claims={}) {
+    const header = btoa(JSON.stringify({
+        alg: 'HS256',
+        typ: 'JWT',
+    }));
+    const payload = btoa(JSON.stringify(claims));
+    const signature = btoa('<NOT IMPLEMENTED>');
+
+    return `${header}.${payload}.${signature}`
+};
+
+
+describe('Auth disabled', () => {
+
+    test('is_protected()', () => {
+        const res = Auth.is_protected();
+        expect(res).toBe(false);
+    });
+
+});
+
+describe('Auth.init()', () => {
+
+    const oldProviderUri = process.env.REACT_APP_JWT_PROVIDER_URI;
+    const oldXMLHttpRequest = window.XMLHttpRequest;
+
+    beforeEach(() => {
+        process.env.REACT_APP_JWT_PROVIDER_URI = MOCK_ENDPOINT;
+        window.fetch = mockFetchApi;
+        LocalStorage.clear();
+    });
+
+    afterAll(() => {
+        process.env.REACT_APP_JWT_PROVIDER_URI = oldProviderUri;
+        window.fetch = _cached_fetch;
+        LocalStorage.clear();
+    });
+
+    test('is_protected()', () => {
+        const res = Auth.is_protected();
+        expect(res).toBe(true);
+    });
+
+    test('init(): no tokens stored', () => {
+        return Auth.init().then((res) => {
+            expect(res).toBe(null);
+        });
+    });
+
+    test('init(): access token expired', () => {
+        // mock data
+        LocalStorage.set('ss_access_token', create_jwt_token({ exp: timestamp(-ONE_HOUR), debug: 'access expired' }));
+
+        return Auth.init().then((res) => {
+            expect(res).toBe(null);
+        });
+    });
+
+    test('init(): access token missing, refresh token expired', () => {
+        // mock data
+        LocalStorage.set('ss_refresh_token', create_jwt_token({ exp: timestamp(-ONE_HOUR), debug: 'refresh expired' }));
+
+        return Auth.init().then((res) => {
+            expect(res).toBe(null);
+        });
+    });
+
+    test('init(): access token un-expired', () => {
+        // mock data
+        LocalStorage.set('ss_access_token', create_jwt_token({ exp: timestamp(ONE_HOUR), debug: 'access' }));
+        // control value
+        const username = `USER_${timestamp()}`;
+        LocalStorage.set('mock_username', username);
+
+        return Auth.init().then((res) => {
+            expect(Object.prototype.toString.call(res)).toBe('[object Object]');
+            expect(res.username).toBe(username);
+        });
+    });
+
+    test('init(): access token missing, refresh token un-expired', () => {
+        // mock data
+        LocalStorage.set('ss_refresh_token', create_jwt_token({ exp: timestamp(ONE_HOUR), debug: 'refresh' }));
+        // control value
+        const username = `USER_${timestamp()}`;
+        LocalStorage.set('mock_username', username);
+
+        return Auth.init().then((res) => {
+            expect(Object.prototype.toString.call(res)).toBe('[object Object]');
+            expect(res.username).toBe(username);
+        });
+    });
+
+    test('init(none): access token expired, refresh token expired', () => {
+        // mock data
+        LocalStorage.set('ss_access_token', create_jwt_token({ exp: timestamp(-ONE_HOUR), debug: 'access expired' }));
+        LocalStorage.set('ss_refresh_token', create_jwt_token({ exp: timestamp(-ONE_HOUR), debug: 'refresh expired' }));
+
+        return Auth.init().then((res) => {
+            expect(res).toBe(null);
+        });
+    });
+
+    test('init(refresh): access token expired, refresh token un-expired', () => {
+        // mock data
+        LocalStorage.set('ss_access_token', create_jwt_token({ exp: timestamp(-ONE_HOUR), debug: 'access expired' }));
+        LocalStorage.set('ss_refresh_token', create_jwt_token({ exp: timestamp(ONE_HOUR), debug: 'refresh' }));
+        // control value
+        const username = `USER_${timestamp()}`;
+        LocalStorage.set('mock_username', username);
+
+        return Auth.init().then((res) => {
+            expect(Object.prototype.toString.call(res)).toBe('[object Object]');
+            expect(res.username).toBe(username);
+        });
+    });
+
+    test('init(access): access token un-expired, refresh token un-expired', () => {
+        // mock data
+        LocalStorage.set('ss_access_token', create_jwt_token({ exp: timestamp(ONE_HOUR), debug: 'access' }));
+        LocalStorage.set('ss_refresh_token', create_jwt_token({ exp: timestamp(ONE_HOUR), debug: 'refresh' }));
+        // control value
+        const username = `USER_${timestamp()}`;
+        LocalStorage.set('mock_username', username);
+
+        return Auth.init().then((res) => {
+            expect(Object.prototype.toString.call(res)).toBe('[object Object]');
+            expect(res.username).toBe(username);
+        });
     });
 
 });
